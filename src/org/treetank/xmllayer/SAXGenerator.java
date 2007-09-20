b94a9457d71255cf1707443ae1810a73d90e5c17
kramis <kramis@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2007-09-20 08:40:26 +0000

    Changed license to GPLv3.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@2962 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/org/treetank/xmllayer/SAXGenerator.java
+++ src/org/treetank/xmllayer/SAXGenerator.java
@@ -1,18 +1,22 @@
 /*
- * GPL
+ * TreeTank - Embedded Native XML Database
  * 
- * Copyright (c) 2005.
+ * Copyright (C) 2007 Marc Kramis
  * 
- * University of Konstanz.
- * 
- * Distributed Systems Group.
- * 
- * Database and Information Systems Group.
- * 
- * All rights reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * $Id$
- * 
  */
 
 package org.treetank.xmllayer;
