804be92c1fcbf165da85e529ffb6c25af7dfed35
Marc Kramis <marc.kramis@uni-konstanz.de>
2007-10-24 15:37:24 +0000

    Removed comments from UTF class.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@3232 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- test/org/treetank/utils/UTFTest.java
+++ test/org/treetank/utils/UTFTest.java
@@ -27,37 +27,37 @@ import org.junit.Test;
 
 public class UTFTest {
 
-//  @Test
-//  public void test() {
-//    assertEquals("a", UTF.convert(UTF.convert("a")));
-//    assertEquals("foo", UTF.convert(UTF.convert("foo")));
-//    assertEquals("fö§", UTF.convert(UTF.convert("fö§")));
-//    assertEquals("", UTF.convert(UTF.convert("")));
-//    
-//    //Tests for fastConvert()
-//    assertEquals("a", UTF.fastConvert(UTF.fastConvert("a")));
-//    assertEquals("foo", UTF.fastConvert(UTF.fastConvert("foo")));
-//    assertEquals("fö§", UTF.fastConvert(UTF.fastConvert("fö§")));
-//    assertEquals("", UTF.fastConvert(UTF.fastConvert("")));
-//  }
-//
-//  @Test
-//  public void testEquals() {
-//    assertEquals(false, UTF.equals(UTF.convert("foo"), UTF.convert("foobar")));
-//    assertEquals(false, UTF.equals(UTF.convert("foo"), UTF.convert("bar")));
-//    assertEquals(true, UTF.equals(UTF.convert("foo"), UTF.convert("foo")));
-//
-//    assertEquals(true, UTF.equals("foo", UTF.convert("foo")));
-//    assertEquals(true, UTF.equals(UTF.convert("foo"), "foo"));
-//    assertEquals(true, UTF.equals("foo", "foo"));
-//    
-//    //Tests for fastConvert()
-//    assertEquals(false, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("foobar")));
-//    assertEquals(false, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("bar")));
-//    assertEquals(true, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("foo")));
-//
-//    assertEquals(true, UTF.fastEquals("foo", UTF.fastConvert("foo")));
-//    assertEquals(true, UTF.fastEquals(UTF.fastConvert("foo"), "foo"));
-//  }
+  @Test
+  public void test() {
+    assertEquals("a", UTF.convert(UTF.convert("a")));
+    assertEquals("foo", UTF.convert(UTF.convert("foo")));
+    assertEquals("fö§", UTF.convert(UTF.convert("fö§")));
+    assertEquals("", UTF.convert(UTF.convert("")));
+    
+    //Tests for fastConvert()
+    assertEquals("a", UTF.fastConvert(UTF.fastConvert("a")));
+    assertEquals("foo", UTF.fastConvert(UTF.fastConvert("foo")));
+    assertEquals("fö§", UTF.fastConvert(UTF.fastConvert("fö§")));
+    assertEquals("", UTF.fastConvert(UTF.fastConvert("")));
+  }
+
+  @Test
+  public void testEquals() {
+    assertEquals(false, UTF.equals(UTF.convert("foo"), UTF.convert("foobar")));
+    assertEquals(false, UTF.equals(UTF.convert("foo"), UTF.convert("bar")));
+    assertEquals(true, UTF.equals(UTF.convert("foo"), UTF.convert("foo")));
+
+    assertEquals(true, UTF.equals("foo", UTF.convert("foo")));
+    assertEquals(true, UTF.equals(UTF.convert("foo"), "foo"));
+    assertEquals(true, UTF.equals("foo", "foo"));
+    
+    //Tests for fastCFonvert()
+    assertEquals(false, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("foobar")));
+    assertEquals(false, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("bar")));
+    assertEquals(true, UTF.equals(UTF.fastConvert("foo"), UTF.fastConvert("foo")));
+
+    assertEquals(true, UTF.fastEquals("foo", UTF.fastConvert("foo")));
+    assertEquals(true, UTF.fastEquals(UTF.fastConvert("foo"), "foo"));
+  }
 
 }
