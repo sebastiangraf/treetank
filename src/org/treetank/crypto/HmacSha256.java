933e635cb575f709388e4593440d1ee74b343654
kramis <kramis@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2008-03-03 11:03:08 +0000

    SHA-256 now supporting message sizes being a multiple of four. Extended test coverage for SHA-256.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@3947 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/org/treetank/crypto/HmacSha256.java
+++ src/org/treetank/crypto/HmacSha256.java
@@ -25,11 +25,29 @@ package org.treetank.crypto;
  * <h1>SHA256</h1>
  * 
  * <p>
- * Calculate 32[B] SHA-256 message digest for messages with a size in bytes
- * being a multiple of four.
+ * Calculate 32[B] HMAC-SHA-256 message digest for messages with a size in
+ * bytes being a multiple of four. The key is 32[B]. The block size is 64[B].
  * </p>
  */
 public final class HmacSha256 {
+
+  /** Block size.*/
+  private static final int BLOCK_SIZE = 64;
+
+  /** Digest size. */
+  private static final int DIGEST_SIZE = 32;
+
+  /** Initial round vector H. */
+  private static final int[] H =
+      {
+          0x6a09e667,
+          0xbb67ae85,
+          0x3c6ef372,
+          0xa54ff53a,
+          0x510e527f,
+          0x9b05688c,
+          0x1f83d9ab,
+          0x5be0cd19 };
 
   /** Table of 64 4[B] round constants. */
   private static final int[] K =
@@ -105,7 +123,7 @@ public final class HmacSha256 {
    * @param hh Input and output.
    * @param ww Input.
    */
-  private final void sha(final int[] hh, final int[] ww) {
+  private final void sha256(final int[] hh, final int[] ww) {
 
     int a = hh[0];
     int b = hh[1];
@@ -171,9 +189,129 @@ public final class HmacSha256 {
    * @param digest The resulting 32[B] digest is stored here. Must not be null
    * and 32[B].
    */
-  public final void digest(byte[] message, int offset, int length, byte[] digest) {
+  public final void digest(
+      final byte[] message,
+      final int offset,
+      final int length,
+      final byte[] digest) {
+
+    // --- Setup ---------------------------------------------------------------
+    // Check arguments.
+    checkMessage(message, offset, length);
+    checkDigest(digest);
+
+    // Prepare round variables.
+    final int[] h = new int[8];
+    final int[] w = new int[64];
+    int o = offset;
+    int r = length;
+    int i = 0;
+    int j = 0;
+    int l = 0;
+
+    // Load initial round vector.
+    System.arraycopy(H, 0, h, 0, 8);
+
+    // --- Main Loop -----------------------------------------------------------
+    // Break message into 64[B] chunks for processing.
+    for (i = 0, l = length >> 6; i < l; i++) {
+      // Break 64[B] chunk into 16 4[B] words for processing.
+      for (j = 0; j < 16; j++) {
+        w[j] = loadWord(message, o);
+        o += 4;
+      }
+      // Update digest.
+      sha256(h, w);
+      r -= BLOCK_SIZE;
+    }
+
+    // --- Padding -------------------------------------------------------------
+    // Break last 64[B] chunk into remaining 4[B] words for processing.
+    for (j = 0, l = r >> 2; j < l; j++) {
+      w[j] = loadWord(message, o);
+      o += 4;
+    }
+    // Add one bit followed by seven zero bits.
+    w[j++] = 0x80000000;
+    // Add a second padding block if the first is full.
+    if (r >= 56) {
+      sha256(h, w);
+      j = 0;
+    }
+    // Add zeroes to padding.
+    for (; j < 15; j++) {
+      w[j] = 0;
+    }
+    // Add the number of bytes contained in the message.
+    w[j] = (int) (length << 3);
+    // Update digest.
+    sha256(h, w);
+
+    // --- Teardown ------------------------------------------------------------
+    // Build final digest.
+    buildDigest(h, digest);
+
+  }
+
+  /**
+   * Calculate HMAC-SHA-256 for a given key and message.
+   * 
+   * @param key Key to calculate HMAC-SHA-256 digest for. The key must not be
+   * null and of length 32[B].
+   * @param message Message to calculate HMAC-SHA-256 digest for. The message
+   * must not be null.
+   * @param offset Offset to start digest calculation with.
+   * @param length Length of message to digest in bytes. 
+   * Must be a multiple of four.
+   * @param digest The resulting 32[B] digest is stored here. Must not be null
+   * and 32[B].
+   */
+  public final void digest(
+      final byte[] key,
+      final byte[] message,
+      final int offset,
+      final int length,
+      final byte[] digest) {
 
     // Check arguments.
+    checkKey(key);
+    checkMessage(message, offset, length);
+    checkDigest(digest);
+
+    final byte[] ipad = new byte[64];
+    final byte[] opad = new byte[64];
+
+    // First half of 64[B] chunk.
+    for (int i = 0; i < 32; i++) {
+      ipad[i] = (byte) (0x36 ^ key[i]);
+      opad[i] = (byte) (0x5c ^ key[i]);
+    }
+
+    // Second half of 64[B] chunk.
+    for (int i = 32; i < 64; i++) {
+      ipad[i] = (byte) (0x36);
+      opad[i] = (byte) (0x5c);
+    }
+
+    //return hash(opad || hash(ipad || message))
+
+    // Inner hash
+
+    // Outer hash
+  }
+
+  private final void checkKey(final byte[] key) {
+    if (key == null || (key.length != 32)) {
+      throw new IllegalArgumentException("Key="
+          + key
+          + " does not comply with specification.");
+    }
+  }
+
+  private final void checkMessage(
+      final byte[] message,
+      final int offset,
+      final int length) {
     if ((message == null)
         || (offset < 0)
         || (length < 0)
@@ -187,56 +325,30 @@ public final class HmacSha256 {
           + length
           + " does not comply with specification.");
     }
+  }
 
-    if (digest == null || (digest.length != 32)) {
+  private final void checkDigest(final byte[] digest) {
+    if (digest == null || (digest.length != DIGEST_SIZE)) {
       throw new IllegalArgumentException("Digest="
           + digest
           + " does not comply with specification.");
     }
+  }
 
-    final int[] h =
-        new int[] {
-            0x6a09e667,
-            0xbb67ae85,
-            0x3c6ef372,
-            0xa54ff53a,
-            0x510e527f,
-            0x9b05688c,
-            0x1f83d9ab,
-            0x5be0cd19 };
-
-    final int[] w = new int[64];
-
-    // Process all n 64[B] message chunks
-    for (int i = 0, o = offset, l = length >> 6; i < l; i++) {
-      // Break chunk into 16 4[B] words.
-      for (int j = 0; j < 16; j++) {
-        w[j] =
-            message[o++] << 24
-                | (message[o++] & 0xFF) << 16
-                | (message[o++] & 0xFF) << 8
-                | (message[o++] & 0xFF);
-      }
-
-      // Digest chunk.
-      sha(h, w);
-    }
-
-    // Process the final padding. TODO Support length % 4.
-    w[0] = 0x80000000;
-    for (int i = 1; i < 15; i++) {
-      w[i] = 0;
-    }
-    w[15] = (int) (length << 3);
-    sha(h, w);
-
-    // Build digest.
+  private final void buildDigest(final int[] h, final byte[] digest) {
     for (int i = 0, o = 0; i < 8; i++) {
       digest[o++] = (byte) (h[i] >>> 24);
       digest[o++] = (byte) (h[i] >>> 16);
       digest[o++] = (byte) (h[i] >>> 8);
       digest[o++] = (byte) h[i];
     }
+  }
+
+  private final int loadWord(final byte[] message, final int offset) {
+    return message[offset] << 24
+        | (message[offset + 1] & 0xFF) << 16
+        | (message[offset + 2] & 0xFF) << 8
+        | (message[offset + 3] & 0xFF);
   }
 
 }
--- test/org/treetank/crypto/HmacSha256Test.java
+++ test/org/treetank/crypto/HmacSha256Test.java
@@ -34,17 +34,175 @@ public class HmacSha256Test {
   /** Hexadecimal digits. */
   private static final char[] HEX_DIGITS = "0123456789ABCDEF".toCharArray();
 
+  /**
+   * Test parameter checks with SHA-256.
+   */
+  @Test
+  public void testSha256Parameter() {
+    final HmacSha256 hmac = new HmacSha256();
+    final byte[] message = new byte[64];
+    final byte[] digest = new byte[32];
+
+    try {
+      hmac.digest(null, 0, 64, digest);
+      Assert.fail("Must not accept null message.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, -33, 64, digest);
+      Assert.fail("Must not accept negative offsets.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, 0, -33, digest);
+      Assert.fail("Must not accept negative lengths.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, 0, 33, digest);
+      Assert.fail("Must not accept lengths not being a multiple of 4.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, 0, 333, digest);
+      Assert.fail("Must not accept lengths exceeding the message size.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, 0, 64, null);
+      Assert.fail("Must not accept null digest.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+    try {
+      hmac.digest(message, 0, 64, new byte[33]);
+      Assert.fail("Must not accept digest with a size not equal to 32.");
+    } catch (IllegalArgumentException e) {
+      // Must throw IllegalArgumentException.
+    }
+
+  }
+
+  /**
+   * Test SHA-256 implementation. Test vectors applied to an empty array with
+   * length being a multiple of 4. All test vectors verified with
+   * https://www.cosic.esat.kuleuven.be/nessie/.
+   */
   @Test
   public void testSha256() {
     final HmacSha256 hmac = new HmacSha256();
     final byte[] message = new byte[64];
     final byte[] digest = new byte[32];
 
+    hmac.digest(message, 0, 0, digest);
+    Assert.assertEquals(
+        "E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 4, digest);
+    Assert.assertEquals(
+        "DF3F619804A92FDB4057192DC43DD748EA778ADC52BC498CE80524C014B81119",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 8, digest);
+    Assert.assertEquals(
+        "AF5570F5A1810B7AF78CAF4BC70A660F0DF51E42BAF91D4DE5B2328DE0E83DFC",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 12, digest);
+    Assert.assertEquals(
+        "15EC7BF0B50732B49F8228E07D24365338F9E3AB994B00AF08E5A3BFFE55FD8B",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 16, digest);
+    Assert.assertEquals(
+        "374708FFF7719DD5979EC875D56CD2286F6D3CF7EC317A3B25632AAB28EC37BB",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 20, digest);
+    Assert.assertEquals(
+        "DE47C9B27EB8D300DBB5F2C353E632C393262CF06340C4FA7F1B40C4CBD36F90",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 24, digest);
+    Assert.assertEquals(
+        "9D908ECFB6B256DEF8B49A7C504E6C889C4B0E41FE6CE3E01863DD7B61A20AA0",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 28, digest);
+    Assert.assertEquals(
+        "3ADDFB141CD7C9C4C6543A82191A3707AC29C7A041217782E61D4D91C691AEE8",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 32, digest);
+    Assert.assertEquals(
+        "66687AADF862BD776C8FC18B8E9F8E20089714856EE233B3902A591D0D5F2925",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 36, digest);
+    Assert.assertEquals(
+        "6DB65FD59FD356F6729140571B5BCD6BB3B83492A16E1BF0A3884442FC3C8A0E",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 40, digest);
+    Assert.assertEquals(
+        "2C34CE1DF23B838C5ABF2A7F6437CCA3D3067ED509FF25F11DF6B11B582B51EB",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 44, digest);
+    Assert.assertEquals(
+        "85759B3811FF7DC47B03792AC85317BE51431A3F9E01DCAFCE317ED736A391B0",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 48, digest);
+    Assert.assertEquals(
+        "17B0761F87B081D5CF10757CCC89F12BE355C70E2E29DF288B65B30710DCBCD1",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 52, digest);
+    Assert.assertEquals(
+        "7955CB2DE90DD9EFC6DF9FDBF5F5D10C114F4135A9A6B52DB1003BE749E32F7A",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 56, digest);
+    Assert.assertEquals(
+        "D4817AA5497628E7C77E6B606107042BBBA3130888C5F47A375E6179BE789FBB",
+        toHexString(digest));
+
+    hmac.digest(message, 0, 60, digest);
+    Assert.assertEquals(
+        "5DCC1B5872DD9FF1C234501F1FEFDA01F664164E1583C3E1BB3DBEA47588AB31",
+        toHexString(digest));
+
     hmac.digest(message, 0, 64, digest);
     Assert.assertEquals(
         "F5A5FD42D16A20302798EF6ED309979B43003D2320D9F0E8EA9831A92759FB4B",
         toHexString(digest));
   }
+
+  //  @Test
+  //  public void testHmacSha256() {
+  //    final HmacSha256 hmac = new HmacSha256();
+  //    final byte[] key = new byte[32];
+  //    final byte[] message = new byte[64];
+  //    final byte[] digest = new byte[32];
+  //
+  //    hmac.digest(key, message, 0, 64, digest);
+  //    Assert.assertEquals(
+  //        "F5A5FD42D16A20302798EF6ED309979B43003D2320D9F0E8EA9831A92759FB4B",
+  //        toHexString(digest));
+  //  }
 
   /**
    * Convert byte array to hexadecimal digits.
