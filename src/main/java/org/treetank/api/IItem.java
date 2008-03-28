a8d975fdf229dc11b48c4d79e3b19afe146ed76a
scherer <scherer@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2008-03-28 12:56:27 +0000

    Added methods to the interface that shall be common to all item types
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4034 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/main/java/org/treetank/api/IItem.java
+++ src/main/java/org/treetank/api/IItem.java
@@ -1,5 +1,60 @@
 package org.treetank.api;
 
+import org.treetank.nodelayer.AbstractNode;
+
 public interface IItem {
+
+  long getNodeKey();
+
+  long getParentKey();
+
+  long getFirstChildKey();
+
+  long getLeftSiblingKey();
+
+  long getRightSiblingKey();
+
+  AbstractNode getAttribute(int index);
+
+  boolean hasParent();
+  
+  boolean hasFirstChild();
+  
+  boolean hasLeftSibling();
+  
+  boolean hasRightSibling();
+
+
+  byte[] getValue();
+
+  long getChildCount();
+
+  int getAttributeCount();
+
+  int getNamespaceCount();
+
+  AbstractNode getNamespace(int index);
+
+  int getKind();
+
+  boolean isDocumentRoot();
+
+  boolean isElement();
+
+  boolean isAttribute();
+
+  boolean isText();
+
+  boolean isFullText();
+
+  boolean isFullTextLeaf();
+
+  boolean isFullTextRoot();
+
+  int getNameKey();
+
+  int getURIKey();
+
+  int getValueType();
 
 }
