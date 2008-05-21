b74cd711dbb6f5fede0fb6f79134806aa32f9cbf
kramis <kramis@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2008-05-21 08:48:42 +0000

    Added simple test case for ItemList.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4174 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ src/test/java/org/treetank/sessionlayer/ItemListTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2007, Marc Kramis
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * $Id: AttributeAndNamespaceTest.java 4017 2008-03-26 16:59:23Z kramis $
+ */
+
+package org.treetank.sessionlayer;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.treetank.api.IItemList;
+import org.treetank.api.IReadTransaction;
+import org.treetank.api.ISession;
+import org.treetank.api.IWriteTransaction;
+import org.treetank.nodelayer.TextNode;
+import org.treetank.utils.TestDocument;
+
+public class ItemListTest {
+
+  public static final String PATH =
+      "target" + File.separator + "tnk" + File.separator + "ItemListTest.tnk";
+
+  @Before
+  public void setUp() {
+    Session.removeSession(PATH);
+  }
+
+  @Test
+  public void testAddItem() throws IOException {
+
+    final IItemList itemList = new ItemList();
+
+    final ISession session = Session.beginSession(PATH);
+    final IWriteTransaction wtx = session.beginWriteTransaction();
+    TestDocument.create(wtx);
+    wtx.commit();
+    wtx.close();
+
+    itemList.addItem(new TextNode(-2, 0, -1, -1, 0, null));
+
+    final IReadTransaction rtx = session.beginReadTransaction(itemList);
+    rtx.moveTo(2);
+    Assert.assertEquals(1, rtx.getAttributeCount());
+    rtx.moveTo(-2);
+    Assert.assertEquals(-2, rtx.getNodeKey());
+    rtx.close();
+
+    session.close();
+
+  }
+
+}
