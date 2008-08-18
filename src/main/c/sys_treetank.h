83322fecd1df30b8507eda52fc587df9baa8e0b0
Marc Kramis <marc.kramis@uni-konstanz.de>
2008-08-18 13:35:28 +0000

    Extracted includes and constants to header file.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4329 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ src/main/c/sys_treetank.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2008, Marc Kramis (Ph.D. Thesis), University of Konstanz
+ * 
+ * Patent Pending.
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for non-
+ * commercial use with or without fee is hereby granted, provided that the 
+ * above copyright notice, the patent notice, and this permission notice
+ * appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * $Id$
+ */
+ 
+/* --- Includes. ------------------------------------------------------------ */
+
+#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/proc.h>
+#include <sys/mount.h>
+#include <sys/syscallargs.h>
+#include <sys/ioctl.h>
+#include <sys/mbuf.h>
+#include <crypto/cryptodev.h>
+
+/* --- Constants. ----------------------------------------------------------- */
+
+#define TT_COMPRESSION_ALGORITHM CRYPTO_LZS_COMP
+
+#define TT_COMPRESS 1
+#define TT_DECOMPRESS 0
+
+#define TT_OK 0
+#define TT_ERROR 1
+
+#define TT_NULL_SESSION 0
