81f40c82d3d35d5f6a42acbc204765268ff91f94
Marc Kramis <marc.kramis@uni-konstanz.de>
2008-08-25 07:40:39 +0000

    Added bench source folder.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4378 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ src/bench/java/org/treetank/bench/AxisStepBenchmark.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (c) 2008, Marc Kramis (Ph.D. Thesis), University of Konstanz
+ * 
+ * Patent Pending.
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for non-
+ * commercial use with or without fee is hereby granted, provided that the 
+ * above copyright notice, the patent notice, and this permission notice
+ * appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * $Id$
+ */
+
+package org.treetank.bench;
+
+import java.io.File;
+
+import org.perfidix.Bench;
+import org.perfidix.BenchClass;
+import org.perfidix.Benchmark;
+import org.perfidix.visitor.AsciiTable;
+import org.treetank.api.IReadTransaction;
+import org.treetank.api.ISession;
+import org.treetank.axislayer.DescendantAxis;
+import org.treetank.sessionlayer.SessionConfiguration;
+import org.treetank.xmllayer.XMLShredder;
+
+@BenchClass(runs = 1)
+public class AxisStepBenchmark {
+
+  public final static int TASKS = 3;
+
+  public final static String XML_PATH = "xml/shakespeare.xml";
+
+  public final static String TNK_PATH = "tnk/shakespeare.tnk";
+
+  public final static byte[] TNK_KEY = null; // "1234567812345678".getBytes();
+
+  public final static boolean TNK_CHECKSUM = false;
+
+  private SessionConfiguration mSessionConfiguration;
+
+  @Bench(runs = 1)
+  public void benchShred() throws Exception {
+
+    new File(TNK_PATH).delete();
+    mSessionConfiguration =
+        new SessionConfiguration(TNK_PATH, TNK_KEY, TNK_CHECKSUM);
+    XMLShredder.shred(XML_PATH, mSessionConfiguration);
+  }
+
+  // @Bench
+  // public void benchTreeTankDescendant() throws Exception {
+  //
+  // final ISession session = Session.beginSession(mSessionConfiguration);
+  // final ExecutorService executor = Executors.newFixedThreadPool(TASKS);
+  // executor.execute(new DescendantStepTask(session));
+  // executor.shutdown();
+  // executor.awaitTermination(1000000, TimeUnit.SECONDS);
+  // session.close();
+  // }
+  //
+  // @Bench
+  // public void benchRandom() throws Exception {
+  //
+  // final ISession session = Session.beginSession(mSessionConfiguration);
+  // final IReadTransaction rtx = session.beginReadTransaction();
+  // final Random r = new Random();
+  //
+  // for (int i = 0; i < 10; i++) {
+  // rtx.moveTo(r.nextInt((int) rtx.getNodeCount()));
+  // }
+  //
+  // rtx.close();
+  // session.close();
+  // }
+  //
+  // @Bench
+  // public void benchConcurrentTreeTankDescendant() throws Exception {
+  //
+  // final ISession session = Session.beginSession(mSessionConfiguration);
+  // final ExecutorService executor = Executors.newFixedThreadPool(TASKS);
+  // try {
+  //
+  // for (int i = 0; i < TASKS; i++) {
+  // executor.execute(new DescendantStepTask(session));
+  // }
+  //
+  // executor.shutdown();
+  // executor.awaitTermination(1000000, TimeUnit.SECONDS);
+  //
+  // } catch (Exception e) {
+  // e.printStackTrace();
+  // }
+  // session.close();
+  // }
+  //
+  // @Bench
+  // public void benchTreeTankChild() throws Exception {
+  //
+  // final ISession session = Session.beginSession(mSessionConfiguration);
+  // final IReadTransaction rtx = session.beginReadTransaction();
+  // for (final long key : new ChildAxis(rtx)) {
+  // // Do nothing.
+  // }
+  // rtx.close();
+  // session.close();
+  // }
+
+  public static void main(final String[] args) {
+
+    System.out
+        .println("$Id:AxisStepBenchmark.java 1617 2006-10-12 17:32:13Z kramis $");
+    try {
+      Benchmark a = new Benchmark();
+      AxisStepBenchmark bench = new AxisStepBenchmark();
+      a.add(bench);
+      org.perfidix.Result r = a.run();
+      AsciiTable v = new AsciiTable();
+      v.visit(r);
+      System.out.println(v.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private class DescendantStepTask implements Runnable {
+
+    private final IReadTransaction mRTX;
+
+    public DescendantStepTask(final ISession session) throws Exception {
+
+      mRTX = session.beginReadTransaction();
+    }
+
+    public void run() {
+
+      try {
+        for (final long key : new DescendantAxis(mRTX)) {
+          // Do nothing
+        }
+        mRTX.close();
+      } catch (Exception e) {
+        throw new IllegalStateException(e);
+      }
+    }
+  }
+
+}
