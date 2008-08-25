4a428bc75d7d575dd656fbdfd5c59f02eaab23bc
kramis <kramis@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2008-08-25 21:13:04 +0000

    Dropped unused jars from classpath.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4396 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/main/openbsd/shred.sh
+++ src/main/openbsd/shred.sh
@@ -6,9 +6,6 @@
     -classpath \
 /treetank/service/wstx-asl.jar:\
 /treetank/service/stax-api.jar:\
-/treetank/service/jetty-util.jar:\
-/treetank/service/jetty.jar:\
-/treetank/service/servlet-api.jar:\
 /treetank/service/backport-util-concurrent.jar:\
 /treetank/service/treetank.jar:\
     org.treetank.xmllayer.XMLShredder $1 $2
--- src/main/openbsd/test.sh
+++ src/main/openbsd/test.sh
@@ -4,11 +4,6 @@
     -Djava.library.path=\
 /treetank/service\
     -classpath \
-/treetank/service/wstx-asl.jar:\
-/treetank/service/stax-api.jar:\
-/treetank/service/jetty-util.jar:\
-/treetank/service/jetty.jar:\
-/treetank/service/servlet-api.jar:\
 /treetank/service/backport-util-concurrent.jar:\
 /treetank/service/treetank.jar:\
     org.treetank.service.TestTreeTankService
