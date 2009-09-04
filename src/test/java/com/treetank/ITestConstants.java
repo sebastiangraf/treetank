e013abb65c541c472ba19cfbdcb1d1d7be01c49d
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2009-09-04 13:15:06 +0000

    set some SessionTest-testcases to ignore and moved to perfidix 3.4
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4845 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/test/java/com/treetank/ITestConstants.java
+++ src/test/java/com/treetank/ITestConstants.java
@@ -4,9 +4,25 @@ import java.io.File;
 
 public interface ITestConstants {
 
-	public static final String PATH1 = "target" + File.separator + "tnk"
+	public static final String PATH1 = "/tmp" + File.separator + "tnk"
 			+ File.separator + "path1";
 
-	public static final String PATH2 = "target" + File.separator + "tnk"
+	public static final String PATH2 = "/tmp" + File.separator + "tnk"
 			+ File.separator + "path2";
+
+	public static final String NON_EXISTING_PATH = "/tmp" + File.separator
+			+ "tnk" + File.separator + "NonExistingSessionTest";
+
+	public static final String TEST_INSERT_CHILD_PATH = "/tmp" + File.separator
+			+ "tnk" + File.separator + "InsertChildSessionTest";
+
+	public static final String TEST_REVISION_PATH = "/tmp" + File.separator
+			+ "tnk" + File.separator + "RevisionSessionTest";
+
+	public static final String TEST_SHREDDED_REVISION_PATH = "/tmp"
+			+ File.separator + "tnk" + File.separator
+			+ "ShreddedRevisionSessionTest";
+
+	public static final String TEST_EXISTING_PATH = "/tmp" + File.separator
+			+ "tnk" + File.separator + "ExistingSessionTest";
 }
