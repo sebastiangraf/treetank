dfcced3af6d80b8abc2c04b652c60ff3ad77fa4d
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2009-10-05 07:08:11 +0000

    io-package is pmd-conform
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4868 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/main/java/com/treetank/io/berkeley/BerkeleyKey.java
+++ src/main/java/com/treetank/io/berkeley/BerkeleyKey.java
@@ -5,35 +5,78 @@ import com.treetank.io.ITTSink;
 import com.treetank.io.ITTSource;
 import com.treetank.io.KeyFactory;
 
+/**
+ * Key for reference the data in the berkeley-db. The key is also the
+ * soft-reference of the pages regarading the PageReference.
+ * 
+ * @author Sebastian Graf, University of Konstanz
+ * 
+ */
 public class BerkeleyKey extends AbstractKey {
-	public BerkeleyKey(final ITTSource in) {
-		super(in.readLong());
-	}
 
-	public BerkeleyKey(final long key) {
-		super(key);
-	}
+    /**
+     * Public constructor
+     * 
+     * @param input
+     *            base for the key (coming from the db)
+     */
+    public BerkeleyKey(final ITTSource input) {
+        super(input.readLong());
+    }
 
-	public static final BerkeleyKey getPropsKey() {
-		return new BerkeleyKey(-3);
-	}
+    /**
+     * Public constructor.
+     * 
+     * @param key
+     *            , coming from the application
+     */
+    public BerkeleyKey(final long key) {
+        super(key);
+    }
 
-	public static final BerkeleyKey getDataInfoKey() {
-		return new BerkeleyKey(-2);
-	}
+    /**
+     * Static method to get the key for the <code>StorageProperties</code>.
+     * 
+     * @return the key for the
+     */
+    public static final BerkeleyKey getPropsKey() {
+        return new BerkeleyKey(-3);
+    }
 
-	public static final BerkeleyKey getFirstRevKey() {
-		return new BerkeleyKey(-1);
-	}
+    /**
+     * Static method to get the key about the information about the last
+     * nodepagekey given.
+     * 
+     * @return the key for the last nodepage key
+     */
+    public static final BerkeleyKey getDataInfoKey() {
+        return new BerkeleyKey(-2);
+    }
 
-	@Override
-	public long getIdentifier() {
+    /**
+     * Static method to get the key about the first reference of the Nodepages
+     * 
+     * @return the key for the first nodepage
+     */
+    public static final BerkeleyKey getFirstRevKey() {
+        return new BerkeleyKey(-1);
+    }
 
-		return super.getKeys()[0];
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getIdentifier() {
 
-	public void serialize(final ITTSink out) {
-		out.writeInt(KeyFactory.BERKELEYKIND);
-		super.serialize(out);
-	}
+        return super.getKeys()[0];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void serialize(final ITTSink out) {
+        out.writeInt(KeyFactory.BERKELEYKIND);
+        super.serialize(out);
+    }
 }
--- src/main/java/com/treetank/io/berkeley/BerkeleyReader.java
+++ src/main/java/com/treetank/io/berkeley/BerkeleyReader.java
@@ -9,29 +9,59 @@ import com.sleepycat.je.OperationStatus;
 import com.sleepycat.je.Transaction;
 import com.treetank.io.IReader;
 import com.treetank.io.StorageProperties;
+import com.treetank.io.TreetankIOException;
 import com.treetank.page.AbstractPage;
 import com.treetank.page.PageReference;
 
+/**
+ * This class represents an reading instance of the Treetank-Application
+ * implementing the {@link IReader}-interface.
+ * 
+ * @author Sebastian Graf, University of Konstanz
+ * 
+ */
 public class BerkeleyReader implements IReader {
 
-    private final Database mDatabase;
+    /** Link to the {@link Database} */
+    private transient final Database mDatabase;
 
-    private final Transaction mTxn;
+    /** Link to the {@link Transaction} */
+    private transient final Transaction mTxn;
 
-    public BerkeleyReader(final Environment env, final Database database,
-            final Transaction txn) {
+    /**
+     * Constructor.
+     * 
+     * @param database
+     *            to be connected to
+     * @param txn
+     *            transaction to be used
+     */
+    public BerkeleyReader(final Database database, final Transaction txn) {
         mTxn = txn;
         mDatabase = database;
 
     }
 
+    /**
+     * Constructor
+     * 
+     * @param env
+     *            to be used
+     * @param database
+     *            to be connected to
+     * @throws DatabaseException
+     *             if something weird happens
+     */
     public BerkeleyReader(final Environment env, final Database database)
             throws DatabaseException {
-        this(env, database, env.beginTransaction(null, null));
+        this(database, env.beginTransaction(null, null));
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public StorageProperties getProps() {
+    public StorageProperties getProps() throws TreetankIOException {
         try {
 
             final DatabaseEntry keyEntry = new DatabaseEntry();
@@ -45,38 +75,44 @@ public class BerkeleyReader implements IReader {
 
             return props;
 
-        } catch (final DatabaseException e) {
-            throw new RuntimeException(e);
+        } catch (final DatabaseException exc) {
+            throw new TreetankIOException(exc);
         }
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public AbstractPage read(PageReference<? extends AbstractPage> pageReference) {
+    public AbstractPage read(
+            final PageReference<? extends AbstractPage> pageReference)
+            throws TreetankIOException {
         final DatabaseEntry valueEntry = new DatabaseEntry();
         final DatabaseEntry keyEntry = new DatabaseEntry();
 
         BerkeleyFactory.KEY.objectToEntry(pageReference.getKey(), keyEntry);
 
+        AbstractPage page = null;
         try {
             final OperationStatus status = mDatabase.get(mTxn, keyEntry,
                     valueEntry, LockMode.DEFAULT);
             if (status == OperationStatus.SUCCESS) {
-                final AbstractPage page = BerkeleyFactory.PAGE_VAL_B
-                        .entryToObject(valueEntry);
+                page = BerkeleyFactory.PAGE_VAL_B.entryToObject(valueEntry);
 
-                return page;
-            } else {
-                return null;
             }
-        } catch (final DatabaseException e) {
-            throw new RuntimeException(e);
+            return page;
+        } catch (final DatabaseException exc) {
+            throw new TreetankIOException(exc);
         }
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public PageReference<?> readFirstReference() {
+    public PageReference<?> readFirstReference() throws TreetankIOException {
         final DatabaseEntry valueEntry = new DatabaseEntry();
         final DatabaseEntry keyEntry = new DatabaseEntry();
         BerkeleyFactory.KEY.objectToEntry(BerkeleyKey.getFirstRevKey(),
@@ -85,26 +121,27 @@ public class BerkeleyReader implements IReader {
         try {
             final OperationStatus status = mDatabase.get(mTxn, keyEntry,
                     valueEntry, LockMode.DEFAULT);
+            PageReference<?> ref = null;
             if (status == OperationStatus.SUCCESS) {
 
-                final PageReference<?> ref = BerkeleyFactory.FIRST_REV_VAL_B
-                        .entryToObject(valueEntry);
-                return ref;
-            } else {
-                return null;
+                ref = BerkeleyFactory.FIRST_REV_VAL_B.entryToObject(valueEntry);
             }
+            return ref;
         } catch (final DatabaseException e) {
-            throw new RuntimeException(e);
+            throw new TreetankIOException(e);
         }
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void close() {
+    public void close() throws TreetankIOException {
         try {
             mTxn.abort();
-        } catch (DatabaseException e) {
-            throw new RuntimeException(e);
+        } catch (final DatabaseException e) {
+            throw new TreetankIOException(e);
 
         }
     }
