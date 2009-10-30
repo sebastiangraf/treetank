ba8cb169741b3254a5b039ed127f2853e9e04ec0
graf <graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2009-10-30 12:58:25 +0000

    removed further @override-tags
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4915 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/test/java/com/treetank/service/rest/TestResponseWrapper.java
+++ src/test/java/com/treetank/service/rest/TestResponseWrapper.java
@@ -33,118 +33,94 @@ public final class TestResponseWrapper implements HttpServletResponse {
         this("", "", null);
     }
 
-    @Override
     public void addCookie(Cookie arg0) {
     }
 
-    @Override
     public void addDateHeader(String arg0, long arg1) {
     }
 
-    @Override
     public void addHeader(String arg0, String arg1) {
 
     }
 
-    @Override
     public void addIntHeader(String arg0, int arg1) {
 
     }
 
-    @Override
     public boolean containsHeader(String arg0) {
         return false;
     }
 
-    @Override
     public String encodeRedirectURL(String arg0) {
         return null;
     }
 
-    @Override
     public String encodeRedirectUrl(String arg0) {
         return null;
     }
 
-    @Override
     public String encodeURL(String arg0) {
         return null;
     }
 
-    @Override
     public String encodeUrl(String arg0) {
         return null;
     }
 
-    @Override
     public void sendError(int arg0) throws IOException {
 
     }
 
-    @Override
     public void sendError(int arg0, String arg1) throws IOException {
 
     }
 
-    @Override
     public void sendRedirect(String arg0) throws IOException {
 
     }
 
-    @Override
     public void setDateHeader(String arg0, long arg1) {
 
     }
 
-    @Override
     public void setHeader(String arg0, String arg1) {
 
     }
 
-    @Override
     public void setIntHeader(String arg0, int arg1) {
 
     }
 
-    @Override
     public void setStatus(int arg0) {
 
     }
 
-    @Override
     public void setStatus(int arg0, String arg1) {
 
     }
 
-    @Override
     public void flushBuffer() throws IOException {
 
     }
 
-    @Override
     public int getBufferSize() {
         return 0;
     }
 
-    @Override
     public String getCharacterEncoding() {
         return null;
     }
 
-    @Override
     public String getContentType() {
         return null;
     }
 
-    @Override
     public Locale getLocale() {
         return null;
     }
 
-    @Override
     public ServletOutputStream getOutputStream() throws IOException {
         return new ServletOutputStream() {
-            @Override
             public void write(int b) throws IOException {
                 buffer.add((byte) b);
             }
@@ -158,49 +134,40 @@ public final class TestResponseWrapper implements HttpServletResponse {
         assertEquals(newByte, content);
     }
 
-    @Override
     public PrintWriter getWriter() throws IOException {
         return null;
     }
 
-    @Override
     public boolean isCommitted() {
         return false;
     }
 
-    @Override
     public void reset() {
 
     }
 
-    @Override
     public void resetBuffer() {
 
     }
 
-    @Override
     public void setBufferSize(int arg0) {
 
     }
 
-    @Override
     public void setCharacterEncoding(String arg0) {
         assertEquals(this.encoding, arg0);
 
     }
 
-    @Override
     public void setContentLength(int arg0) {
 
     }
 
-    @Override
     public void setContentType(String arg0) {
         assertEquals(this.contentType, arg0);
 
     }
 
-    @Override
     public void setLocale(Locale arg0) {
 
     }
