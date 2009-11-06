ef7dc530c4b053fdb8f0921a52f5e6a6ca490f48
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2009-11-06 12:59:53 +0000

    working with berkeley and inserting huge amounts
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@4941 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- src/main/java/com/treetank/bench/ExtremeInsertTest.java
+++ src/main/java/com/treetank/bench/ExtremeInsertTest.java
@@ -3,10 +3,7 @@ package com.treetank.bench;
 import java.io.File;
 import java.util.Random;
 
-import org.perfidix.Benchmark;
 import org.perfidix.annotation.Bench;
-import org.perfidix.ouput.TabularSummaryOutput;
-import org.perfidix.result.BenchmarkResult;
 
 import com.treetank.api.ISession;
 import com.treetank.api.IWriteTransaction;
@@ -14,7 +11,8 @@ import com.treetank.session.Session;
 
 public class ExtremeInsertTest {
 
-    private static final int NUM_CHARS = 10;
+    private static int NUM_CHARS = 10;
+    private static int ELEMENTS = 100000;
     private static final Random ran = new Random(0l);
     public static String chars = "abcdefghijklmonpqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -27,7 +25,7 @@ public class ExtremeInsertTest {
             final ISession session = Session.beginSession(file);
             IWriteTransaction wtx = session.beginWriteTransaction();
             wtx.insertElementAsFirstChild(getString(), "");
-            for (int i = 0; i < 100000; i++) {
+            for (int i = 0; i < ELEMENTS; i++) {
                 if (ran.nextBoolean()) {
                     wtx.insertElementAsFirstChild(getString(), "");
                 } else {
@@ -45,13 +43,13 @@ public class ExtremeInsertTest {
     }
 
     public static void main(String[] args) {
-        final Benchmark bench = new Benchmark();
-        bench.add(ExtremeInsertTest.class);
-        final BenchmarkResult res = bench.run();
-        final TabularSummaryOutput output = new TabularSummaryOutput();
-        output.visitBenchmark(res);
-
-        // new ExtremeInsertTest().bench();
+        // final Benchmark bench = new Benchmark();
+        // bench.add(ExtremeInsertTest.class);
+        // final BenchmarkResult res = bench.run();
+        // final TabularSummaryOutput output = new TabularSummaryOutput();
+        // output.visitBenchmark(res);
+        ELEMENTS = Integer.parseInt(args[0]);
+        new ExtremeInsertTest().bench();
 
     }
 
