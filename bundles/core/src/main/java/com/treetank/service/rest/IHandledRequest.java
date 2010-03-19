bffdcb3040a8f66275c6bcc5274d48dc0e114a8c
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2010-03-19 09:02:52 +0000

    removed native REST from Georgios moved several Dependencies to jax-rx module
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5273 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/core/src/main/java/com/treetank/service/rest/IHandledRequest.java
+++
@@ -1,9 +0,0 @@
-package com.treetank.service.rest;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface IHandledRequest extends HttpServletRequest {
-
-    void setHandled(final boolean handled);
-
-}
