b1981fc35c34a238da14f78abcfc03befd0e41f1
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2010-05-27 16:26:59 +0000

    Added another Testcase in TestNodeWrapper.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5341 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-saxon/src/test/java/com/treetank/saxon/wrapper/TestNodeWrapper.java
+++ bundles/treetank-saxon/src/test/java/com/treetank/saxon/wrapper/TestNodeWrapper.java
@@ -9,6 +9,7 @@ import net.sf.saxon.Configuration;
 import net.sf.saxon.om.Axis;
 import net.sf.saxon.om.AxisIterator;
 import net.sf.saxon.om.NodeInfo;
+import net.sf.saxon.om.NamespaceIterator.NamespaceNodeImpl;
 import net.sf.saxon.pattern.NameTest;
 import net.sf.saxon.s9api.Processor;
 import net.sf.saxon.trans.XPathException;
@@ -18,7 +19,6 @@ import net.sf.saxon.value.Value;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -31,6 +31,12 @@ import com.treetank.exception.TreetankException;
 import com.treetank.service.xml.XMLShredder;
 import com.treetank.utils.DocumentCreater;
 
+/**
+ * Test implemented methods in NodeWrapper.
+ * 
+ * @author Johannes Lichtenberger, University of Konstanz
+ *
+ */
 public class TestNodeWrapper {
 
   /** Treetank session on Treetank test document. */
@@ -39,26 +45,20 @@ public class TestNodeWrapper {
   /** Document node. */
   private transient NodeWrapper node;
 
-  @BeforeClass
-  public static void setUp() throws TreetankException {
+  @Before
+  public void beforeMethod() throws TreetankException {
     Database.truncateDatabase(TestHelper.PATHS.PATH1.getFile());
-    final IDatabase database =
+    Database.forceCloseDatabase(TestHelper.PATHS.PATH1.getFile());
+    IDatabase database =
         Database.openDatabase(TestHelper.PATHS.PATH1.getFile());
     sessionTest = database.getSession();
     final IWriteTransaction wtx = sessionTest.beginWriteTransaction();
     DocumentCreater.create(wtx);
     wtx.commit();
     wtx.close();
-  }
 
-  @Before
-  public void beforeMethod() throws TreetankException {
     final Processor proc = new Processor(false);
     final Configuration config = proc.getUnderlyingConfiguration();
-
-    final IDatabase database =
-        Database.openDatabase(TestHelper.PATHS.PATH1.getFile());
-    sessionTest = database.getSession();
 
     node =
         (NodeWrapper) new DocumentWrapper(
@@ -162,7 +162,9 @@ public class TestNodeWrapper {
         attribute.getURI(),
         attribute.getLocalPart());
 
+    // Only supported on element nodes.
     node = (NodeWrapper) node.getParent();
+
     TestCase.assertEquals("j", node.getAttributeValue(attribute
         .getFingerprint()));
   }
@@ -218,5 +220,88 @@ public class TestNodeWrapper {
     final NodeInfo baz = (NodeInfo) iterator.next();
 
     TestCase.assertEquals("http://example.org", baz.getBaseURI());
+  }
+
+  @Test
+  public void testGetDeclaredNamespaces() {
+    // Namespace declared.
+    final AxisIterator iterator = node.iterateAxis(Axis.CHILD);
+    node = (NodeWrapper) iterator.next();
+    final int[] namespaces = node.getDeclaredNamespaces(new int[1]);
+
+    node.getNamePool().allocateNamespaceCode("p", "ns");
+    final int expected = node.getNamePool().getNamespaceCode("p", "ns");
+
+    TestCase.assertEquals(expected, namespaces[0]);
+
+    // Namespace not declared (on element node) -- returns zero length array.
+    final AxisIterator iter = node.iterateAxis(Axis.DESCENDANT);
+    node = (NodeWrapper) iter.next();
+    node = (NodeWrapper) iter.next();
+
+    final int[] namesp = node.getDeclaredNamespaces(new int[1]);
+
+    TestCase.assertTrue(namesp.length == 0);
+
+    // Namespace nod declared on other nodes -- return null.
+    final AxisIterator it = node.iterateAxis(Axis.DESCENDANT);
+    node = (NodeWrapper) it.next();
+
+    TestCase.assertNull(node.getDeclaredNamespaces(new int[1]));
+  }
+
+  @Test
+  public void testGetStringValueCS() {
+    // Test on document node.
+    TestCase.assertEquals("oops1foooops2baroops3", node.getStringValueCS());
+
+    // Test on element node.
+    AxisIterator iterator = node.iterateAxis(Axis.DESCENDANT);
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals("oops1foooops2baroops3", node.getStringValueCS());
+
+    // Test on namespace node.
+    iterator = node.iterateAxis(Axis.NAMESPACE);    
+    NamespaceNodeImpl namespace = (NamespaceNodeImpl) iterator.next();
+    
+    while (!"ns".equals(namespace.getStringValueCS()) && namespace != null) {
+      namespace = (NamespaceNodeImpl) iterator.next();
+    }
+    
+    TestCase.assertEquals("ns", namespace.getStringValueCS());
+
+    // Test on attribute node.
+    final NodeWrapper attrib =
+        (NodeWrapper) node.iterateAxis(Axis.ATTRIBUTE).next();
+    TestCase.assertEquals("j", attrib.getStringValueCS());
+
+    // Test on text node.
+    final NodeWrapper text = (NodeWrapper) node.iterateAxis(Axis.CHILD).next();
+    TestCase.assertEquals("oops1", text.getStringValueCS());
+  }
+  
+  @Test
+  public void testGetSiblingPosition() {
+    // Test every node in test document.
+    final AxisIterator iterator = node.iterateAxis(Axis.DESCENDANT);
+    node = (NodeWrapper) iterator.next();
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(0, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(1, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(0, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(1, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(2, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(3, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(0, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(1, node.getSiblingPosition());
+    node = (NodeWrapper) iterator.next();
+    TestCase.assertEquals(4, node.getSiblingPosition());
   }
 }
