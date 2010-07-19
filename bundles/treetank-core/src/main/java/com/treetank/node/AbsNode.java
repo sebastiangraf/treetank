3a831ba3a67538be8c6d624101ef34317e01afc7
bhummiwat.manphadung <bhummiwat.manphadung@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2010-07-19 12:28:45 +0000

    Hashcode functions for nodes implemented and equals fixed
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5468 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/main/java/com/treetank/node/AbsNode.java
+++ bundles/treetank-core/src/main/java/com/treetank/node/AbsNode.java
@@ -18,6 +18,8 @@
 
 package com.treetank.node;
 
+import java.util.Arrays;
+
 import com.treetank.api.IItem;
 import com.treetank.io.ITTSink;
 import com.treetank.settings.EFixed;
@@ -34,210 +36,209 @@ import com.treetank.settings.EFixed;
  */
 public abstract class AbsNode implements IItem, Comparable<AbsNode> {
 
-    /** standard NODE_KEY. */
-    protected static final int NODE_KEY = 0;
+	/** standard NODE_KEY. */
+	protected static final int NODE_KEY = 0;
 
-    /** standard PARENT_KEY */
-    protected static final int PARENT_KEY = 1;
+	/** standard PARENT_KEY */
+	protected static final int PARENT_KEY = 1;
 
-    /** Node key is common to all node kinds. */
-    protected final long[] mData;
+	/** Node key is common to all node kinds. */
+	protected final long[] mData;
 
-    /**
-     * Constructor for inserting node.
-     * 
-     * @param nodeKey
-     *            Key of node.
-     * @param size
-     *            Size of the data.
-     */
-    AbsNode(final long[] builder) {
-        mData = builder;
-    }
+	/**
+	 * Constructor for inserting node.
+	 * 
+	 * @param nodeKey
+	 *            Key of node.
+	 * @param size
+	 *            Size of the data.
+	 */
+	AbsNode(final long[] builder) {
+		mData = builder;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public final long getNodeKey() {
-        return mData[NODE_KEY];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public final long getNodeKey() {
+		return mData[NODE_KEY];
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public boolean hasParent() {
-        return mData[PARENT_KEY] != (Long) EFixed.NULL_NODE_KEY
-                .getStandardProperty();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean hasParent() {
+		return mData[PARENT_KEY] != (Long) EFixed.NULL_NODE_KEY
+				.getStandardProperty();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public long getParentKey() {
-        return mData[PARENT_KEY];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public long getParentKey() {
+		return mData[PARENT_KEY];
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public ENodes getKind() {
-        return ENodes.UNKOWN_KIND;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public ENodes getKind() {
+		return ENodes.UNKOWN_KIND;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public int getNameKey() {
-        return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public int getNameKey() {
+		return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public int getURIKey() {
-        return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public int getURIKey() {
+		return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public int getTypeKey() {
-        return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public int getTypeKey() {
+		return (Integer) EFixed.NULL_INT_KEY.getStandardProperty();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public byte[] getRawValue() {
-        return null;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public byte[] getRawValue() {
+		return null;
+	}
 
-    /**
-     * Serializing the data.
-     * 
-     * @param out
-     *            target to serialize.
-     */
-    public void serialize(final ITTSink out) {
-        for (final long longVal : mData) {
-            out.writeLong(longVal);
-        }
+	/**
+	 * Serializing the data.
+	 * 
+	 * @param out
+	 *            target to serialize.
+	 */
+	public void serialize(final ITTSink out) {
+		for (final long longVal : mData) {
+			out.writeLong(longVal);
+		}
 
-    }
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    public void setNodeKey(final long nodeKey) {
-        mData[NODE_KEY] = nodeKey;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	public void setNodeKey(final long nodeKey) {
+		mData[NODE_KEY] = nodeKey;
+	}
 
-    /**
-     * Setting the parent key.
-     * 
-     * @param parentKey
-     *            the key for the parent.
-     */
-    public void setParentKey(final long parentKey) {
-        mData[PARENT_KEY] = parentKey;
-    }
+	/**
+	 * Setting the parent key.
+	 * 
+	 * @param parentKey
+	 *            the key for the parent.
+	 */
+	public void setParentKey(final long parentKey) {
+		mData[PARENT_KEY] = parentKey;
+	}
 
-    /**
-     * Setting the kind of this node.
-     * 
-     * @param kind
-     *            to be set.
-     */
-    public void setKind(final byte kind) {
-    }
+	/**
+	 * Setting the kind of this node.
+	 * 
+	 * @param kind
+	 *            to be set.
+	 */
+	public void setKind(final byte kind) {
+	}
 
-    /**
-     * Setting the name key for this node.
-     * 
-     * @param nameKey
-     *            to be set.
-     */
-    public void setNameKey(final int nameKey) {
-    }
+	/**
+	 * Setting the name key for this node.
+	 * 
+	 * @param nameKey
+	 *            to be set.
+	 */
+	public void setNameKey(final int nameKey) {
+	}
 
-    /**
-     * Setting the uri for this node.
-     * 
-     * @param uriKey
-     *            to be set.
-     */
-    public void setURIKey(final int uriKey) {
-    }
+	/**
+	 * Setting the uri for this node.
+	 * 
+	 * @param uriKey
+	 *            to be set.
+	 */
+	public void setURIKey(final int uriKey) {
+	}
 
-    /**
-     * Setting the value for this node.
-     * 
-     * @param valueType
-     *            type of value to be set.
-     * @param value
-     *            the value to be set.
-     */
-    public void setValue(final int valueType, final byte[] value) {
-    }
+	/**
+	 * Setting the value for this node.
+	 * 
+	 * @param valueType
+	 *            type of value to be set.
+	 * @param value
+	 *            the value to be set.
+	 */
+	public void setValue(final int valueType, final byte[] value) {
+	}
 
-    /**
-     * Setting the type of this node.
-     * 
-     * @param valueType
-     *            to be set.
-     */
-    public void setType(final int valueType) {
-    }
+	/**
+	 * Setting the type of this node.
+	 * 
+	 * @param valueType
+	 *            to be set.
+	 */
+	public void setType(final int valueType) {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        return (int) mData[NODE_KEY];
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object obj) {
-        return ((obj != null) && (mData[NODE_KEY] == ((AbsNode) obj).mData[NODE_KEY]));
-    }
 
-    /**
-     * {@inheritDoc}
-     */
-    public int compareTo(final AbsNode node) {
-        final long nodeKey = (node).getNodeKey();
-        if (mData[NODE_KEY] < nodeKey) {
-            return -1;
-        } else if (mData[NODE_KEY] == nodeKey) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(mData);
+		return result;
+	}
 
-    @Override
-    public String toString() {
-        return new StringBuilder("\n").append(this.getClass().getName())
-                .append("\n\tnode key: ").append(getNodeKey())
-                .append("\n\tparentKey: ").append(getParentKey()).toString();
-    }
+	@Override
+	public boolean equals(final Object obj) {
+		return this.hashCode()==obj.hashCode();
+	}
 
-    public abstract AbsNode clone();
+	/**
+	 * {@inheritDoc}
+	 */
+	public int compareTo(final AbsNode node) {
+		final long nodeKey = (node).getNodeKey();
+		if (mData[NODE_KEY] < nodeKey) {
+			return -1;
+		} else if (mData[NODE_KEY] == nodeKey) {
+			return 0;
+		} else {
+			return 1;
+		}
+	}
 
-    protected static long[] cloneData(final long[] input) {
-        final long[] data = new long[input.length];
-        System.arraycopy(input, 0, data, 0, data.length);
-        return data;
-    }
+	@Override
+	public String toString() {
+		return new StringBuilder("\n").append(this.getClass().getName())
+				.append("\n\tnode key: ").append(getNodeKey())
+				.append("\n\tparentKey: ").append(getParentKey()).toString();
+	}
 
-    protected static byte[] cloneValue(final byte[] input) {
-        final byte[] value = new byte[input.length];
-        System.arraycopy(input, 0, value, 0, value.length);
-        return value;
-    }
+	public abstract AbsNode clone();
+
+	protected static long[] cloneData(final long[] input) {
+		final long[] data = new long[input.length];
+		System.arraycopy(input, 0, data, 0, data.length);
+		return data;
+	}
+
+	protected static byte[] cloneValue(final byte[] input) {
+		final byte[] value = new byte[input.length];
+		System.arraycopy(input, 0, value, 0, value.length);
+		return value;
+	}
 
 }
--- bundles/treetank-core/src/main/java/com/treetank/node/AttributeNode.java
+++ bundles/treetank-core/src/main/java/com/treetank/node/AttributeNode.java
@@ -18,6 +18,8 @@
 
 package com.treetank.node;
 
+import java.util.Arrays;
+
 import com.treetank.io.ITTSink;
 
 /**
@@ -29,155 +31,163 @@ import com.treetank.io.ITTSink;
  */
 public final class AttributeNode extends AbsNode {
 
-    protected static final int NAME_KEY = 2;
+	protected static final int NAME_KEY = 2;
 
-    protected static final int URI_KEY = 3;
+	protected static final int URI_KEY = 3;
 
-    protected static final int TYPE = 4;
+	protected static final int TYPE = 4;
 
-    protected static final int VALUE_LENGTH = 5;
+	protected static final int VALUE_LENGTH = 5;
 
-    /** Value of attribute. */
-    private byte[] mValue;
+	/** Value of attribute. */
+	private byte[] mValue;
 
-    /**
-     * Creating an attributes
-     * 
-     * @param builder
-     *            long array with data
-     * @param value
-     *            value for the node
-     */
-    AttributeNode(final long[] builder, final byte[] value) {
-        super(builder);
-        mValue = value;
-        mData[VALUE_LENGTH] = value.length;
-    }
+	/**
+	 * Creating an attributes
+	 * 
+	 * @param builder
+	 *            long array with data
+	 * @param value
+	 *            value for the node
+	 */
+	AttributeNode(final long[] builder, final byte[] value) {
+		super(builder);
+		mValue = value;
+		mData[VALUE_LENGTH] = value.length;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getNameKey() {
-        return (int) mData[NAME_KEY];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getNameKey() {
+		return (int) mData[NAME_KEY];
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setNameKey(final int nameKey) {
-        this.mData[NAME_KEY] = nameKey;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setNameKey(final int nameKey) {
+		this.mData[NAME_KEY] = nameKey;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean hasParent() {
-        return true;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean hasParent() {
+		return true;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getURIKey() {
-        return (int) mData[URI_KEY];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getURIKey() {
+		return (int) mData[URI_KEY];
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setURIKey(final int uriKey) {
-        mData[URI_KEY] = uriKey;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setURIKey(final int uriKey) {
+		mData[URI_KEY] = uriKey;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int getTypeKey() {
-        return (int) mData[TYPE];
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int getTypeKey() {
+		return (int) mData[TYPE];
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public byte[] getRawValue() {
-        return mValue;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public byte[] getRawValue() {
+		return mValue;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setValue(final int valueType, final byte[] value) {
-        mData[TYPE] = valueType;
-        mData[VALUE_LENGTH] = value.length;
-        mValue = value;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setValue(final int valueType, final byte[] value) {
+		mData[TYPE] = valueType;
+		mData[VALUE_LENGTH] = value.length;
+		mValue = value;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void setType(final int valueType) {
-        mData[TYPE] = valueType;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setType(final int valueType) {
+		mData[TYPE] = valueType;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public ENodes getKind() {
-        return ENodes.ATTRIBUTE_KIND;
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ENodes getKind() {
+		return ENodes.ATTRIBUTE_KIND;
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void serialize(final ITTSink out) {
-        super.serialize(out);
-        for (final byte byteVal : mValue) {
-            out.writeByte(byteVal);
-        }
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void serialize(final ITTSink out) {
+		super.serialize(out);
+		for (final byte byteVal : mValue) {
+			out.writeByte(byteVal);
+		}
+	}
 
-    public AbsNode clone() {
-        final AbsNode toClone = new AttributeNode(AbsNode.cloneData(mData),
-                AbsNode.cloneValue(mValue));
-        return toClone;
-    }
+	public AbsNode clone() {
+		final AbsNode toClone = new AttributeNode(AbsNode.cloneData(mData),
+				AbsNode.cloneValue(mValue));
+		return toClone;
+	}
 
-    public final static long[] createData(final long nodeKey,
-            final long parentKey, final int nameKey, final int uriKey,
-            final int type) {
-        final long[] data = new long[ENodes.ATTRIBUTE_KIND.getSize()];
-        data[AbsNode.NODE_KEY] = nodeKey;
-        data[AbsNode.PARENT_KEY] = parentKey;
-        data[AttributeNode.NAME_KEY] = nameKey;
-        data[AttributeNode.URI_KEY] = uriKey;
-        data[AttributeNode.TYPE] = type;
-        return data;
-    }
+	public final static long[] createData(final long nodeKey,
+			final long parentKey, final int nameKey, final int uriKey,
+			final int type) {
+		final long[] data = new long[ENodes.ATTRIBUTE_KIND.getSize()];
+		data[AbsNode.NODE_KEY] = nodeKey;
+		data[AbsNode.PARENT_KEY] = parentKey;
+		data[AttributeNode.NAME_KEY] = nameKey;
+		data[AttributeNode.URI_KEY] = uriKey;
+		data[AttributeNode.TYPE] = type;
+		return data;
+	}
 
-    public final static long[] createData(final long nodeKey,
-            final AttributeNode node) {
-        return createData(nodeKey, node.getParentKey(), node.getNameKey(),
-                node.getURIKey(), node.getTypeKey());
-    }
+	public final static long[] createData(final long nodeKey,
+			final AttributeNode node) {
+		return createData(nodeKey, node.getParentKey(), node.getNameKey(),
+				node.getURIKey(), node.getTypeKey());
+	}
 
-    @Override
-    public String toString() {
-        return new StringBuilder(super.toString()).append("\n\tname key: ")
-                .append(getNameKey()).append("\n\turi key: ")
-                .append(getURIKey()).append("\n\ttype: ").append(getTypeKey())
-                .append("\n\tvalue length: ").append(mData[VALUE_LENGTH])
-                .append("\n\tvalue: ").append(new String(mValue)).toString();
-    }
+	@Override
+	public String toString() {
+		return new StringBuilder(super.toString()).append("\n\tname key: ")
+				.append(getNameKey()).append("\n\turi key: ")
+				.append(getURIKey()).append("\n\ttype: ").append(getTypeKey())
+				.append("\n\tvalue length: ").append(mData[VALUE_LENGTH])
+				.append("\n\tvalue: ").append(new String(mValue)).toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Arrays.hashCode(mValue);
+		return result;
+	}
 
 }
