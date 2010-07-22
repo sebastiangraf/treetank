6a70bc5e0075744e75971ca882e47953244cda0e
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2010-07-22 08:27:53 +0000

    ASSIGNED - task 548: Refactoring of Shredder /Serializer https://projects.uni-konstanz.de/issues/show/548
    added package docu
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5478 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ bundles/treetank-core/src/main/java/com/treetank/service/xml/shredder/package-info.java
@@ -0,0 +1,13 @@
+/**
+<h1>Dhredder Package</h1>
+    <p>
+    All class in this package are for getting data into Treetank. 
+    This includes the import of single XML Instances plus the the possibility to shred serveral indepenant versions into Treetank.
+    Furthermore convience classes for jdbc-data are provided.
+    </p>
+ *
+ * @author Marc Kramis, University of Konstanz
+ * @author Sebastian Graf, University of Konstanz
+ */
+package com.treetank.service.xml.shredder;
+
