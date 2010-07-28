df31b6c4a3920f5e20672169b68daf6c07a4a9df
Bhummiwat Manphadung <bhummiwat.manphadung@uni-konstanz.de>
2010-07-28 13:40:49 +0000

    ASSIGNED - task 551: Switching from commons.logging to sl4j.logging https://projects.uni-konstanz.de/issues/show/551
    Adapting LogWrapper plus refactoring LogWrapper methods for wrapping logging operations
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5524 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/main/java/com/treetank/cache/BerkeleyPersistenceCache.java
+++ bundles/treetank-core/src/main/java/com/treetank/cache/BerkeleyPersistenceCache.java
@@ -1,17 +1,18 @@
-/*
- * Copyright (c) 2009, Sebastian Graf (Ph.D. Thesis), University of Konstanz
+/**
+ * Copyright (c) 2010, Distributed Systems Group, University of Konstanz
  * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * THE SOFTWARE IS PROVIDED AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
  * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
  * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
  */
 package com.treetank.cache;
 
@@ -26,6 +27,7 @@ import com.sleepycat.je.LockMode;
 import com.sleepycat.je.OperationStatus;
 import com.treetank.access.DatabaseConfiguration;
 import com.treetank.exception.TreetankIOException;
+import com.treetank.utils.LogWrapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,46 +42,53 @@ import org.slf4j.LoggerFactory;
  */
 public final class BerkeleyPersistenceCache extends AbstractPersistenceCache {
 
-    /** Logger. */
+    /** Logger for determing the log level. */
     private static final Logger LOGGER = LoggerFactory.getLogger(BerkeleyPersistenceCache.class);
 
     /**
-     * Berkeley database
+     * Log wrapper for better output.
      */
-    private transient final Database database;
+    private static final LogWrapper LOGWRAPPER = new LogWrapper(LOGGER);
 
     /**
-     * Berkeley Environment for the database
+     * Berkeley database.
      */
-    private transient final Environment env;
+    private final transient Database mDatabase;
+
+    /**
+     * Berkeley Environment for the database.
+     */
+    private final transient Environment mEnv;
 
     /**
      * Name for the database.
      */
-    private transient final static String NAME = "berkeleyCache";
+    private static final String NAME = "berkeleyCache";
 
     /**
      * Binding for the key, which is the nodepage.
      */
-    private transient final TupleBinding<Long> keyBinding;
+    private final transient TupleBinding<Long> mKeyBinding;
 
     /**
      * Binding for the value which is a page with related Nodes.
      */
-    private transient final NodePageContainerBinding valueBinding;
+    private final transient NodePageContainerBinding mValueBinding;
 
     /**
      * Constructor. Building up the berkeley db and setting necessary settings.
      * 
-     * @param sessionConfig
+     * @param paramSessionConfig
      *            the place where the berkeley db is stored.
-     * @param revision
+     * @param paramRevision
      *            revision number, needed to reconstruct the sliding window in
      *            the correct way
+     * @throws TreetankIOException
+     *             Exception if IO is not successful
      */
-    public BerkeleyPersistenceCache(final DatabaseConfiguration sessionConfig, final long revision)
+    public BerkeleyPersistenceCache(final DatabaseConfiguration paramSessionConfig, final long paramRevision)
         throws TreetankIOException {
-        super(sessionConfig);
+        super(paramSessionConfig);
         try {
 
             /* Create a new, transactional database environment */
@@ -87,23 +96,24 @@ public final class BerkeleyPersistenceCache extends AbstractPersistenceCache {
             config.setAllowCreate(true);
             config.setLocking(false);
             config.setCacheSize(1024 * 1024);
-            env = new Environment(place, config);
+            mEnv = new Environment(place, config);
 
             /* Make a database within that environment */
             final DatabaseConfig dbConfig = new DatabaseConfig();
             dbConfig.setAllowCreate(true);
             dbConfig.setExclusiveCreate(true);
-            database = env.openDatabase(null, NAME, dbConfig);
+            mDatabase = mEnv.openDatabase(null, NAME, dbConfig);
 
-            keyBinding = TupleBinding.getPrimitiveBinding(Long.class);
-            valueBinding = new NodePageContainerBinding();
+            mKeyBinding = TupleBinding.getPrimitiveBinding(Long.class);
+            mValueBinding = new NodePageContainerBinding();
 
             // debug
-            LOGGER.debug(new StringBuilder("Creating new BerkeleyPersistenceCache with revision ").append(
-                revision).append(" and DatabaseConfiguration ").append(sessionConfig.toString()).toString());
+            LOGWRAPPER.debug(
+                "Creating new BerkeleyPersistenceCache with revision {} and DatabaseConfiguration {}",
+                paramRevision, paramSessionConfig);
 
         } catch (final DatabaseException exc) {
-            LOGGER.error(exc.getMessage(), exc);
+            LOGWRAPPER.error(exc);
             throw new TreetankIOException(exc);
 
         }
@@ -117,10 +127,10 @@ public final class BerkeleyPersistenceCache extends AbstractPersistenceCache {
         final DatabaseEntry valueEntry = new DatabaseEntry();
         final DatabaseEntry keyEntry = new DatabaseEntry();
 
-        keyBinding.objectToEntry(key, keyEntry);
-        valueBinding.objectToEntry(page, valueEntry);
+        mKeyBinding.objectToEntry(key, keyEntry);
+        mValueBinding.objectToEntry(page, valueEntry);
         try {
-            database.put(null, keyEntry, valueEntry);
+            mDatabase.put(null, keyEntry, valueEntry);
 
             // debug
             LOGGER.debug(new StringBuilder("Put new BerkeleyPersistenceCache with key ").append(key).append(
@@ -139,11 +149,12 @@ public final class BerkeleyPersistenceCache extends AbstractPersistenceCache {
     @Override
     public void clearPersistent() throws TreetankIOException {
         try {
-            database.close();
-            env.removeDatabase(null, NAME);
-            env.close();
+            mDatabase.close();
+            mEnv.removeDatabase(null, NAME);
+            mEnv.close();
 
             // debug
+
             LOGGER.debug(new StringBuilder("Clear BerkeleyPersistenceCache").toString());
 
         } catch (final DatabaseException exc) {
@@ -159,12 +170,12 @@ public final class BerkeleyPersistenceCache extends AbstractPersistenceCache {
     public NodePageContainer getPersistent(final long key) throws TreetankIOException {
         final DatabaseEntry valueEntry = new DatabaseEntry();
         final DatabaseEntry keyEntry = new DatabaseEntry();
-        keyBinding.objectToEntry(key, keyEntry);
+        mKeyBinding.objectToEntry(key, keyEntry);
         try {
-            final OperationStatus status = database.get(null, keyEntry, valueEntry, LockMode.DEFAULT);
+            final OperationStatus status = mDatabase.get(null, keyEntry, valueEntry, LockMode.DEFAULT);
             NodePageContainer val = null;
             if (status == OperationStatus.SUCCESS) {
-                val = valueBinding.entryToObject(valueEntry);
+                val = mValueBinding.entryToObject(valueEntry);
             }
 
             // debug
