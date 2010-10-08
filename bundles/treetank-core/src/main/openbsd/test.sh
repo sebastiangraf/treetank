4f80e25e2803ce108133327cdc208a22a0bde6a3
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2010-10-08 16:01:48 +0000

    removed useless stuff like bench folder and openbsd-stuff
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5562 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/main/openbsd/test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-/treetank/jre/bin/jamvm -Xms80M -Xmx80M\
-    -Djava.library.path=\
-/treetank/service\
-    -classpath \
-/treetank/service/treetank.jar:\
-    com.treetank.service.rest.TestTreeTankService
