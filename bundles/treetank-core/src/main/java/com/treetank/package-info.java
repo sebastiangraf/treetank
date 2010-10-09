1857cc07f73abd36985784c07faca63b8fdfcf14
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2010-10-09 11:28:42 +0000

    ASSIGNED - task 784: Rolling-Merkle hashed structure https://projects.uni-konstanz.de/issues/show/784
    further implementing of rolling hash + creation of paper-bundle
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5567 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/main/java/com/treetank/package-info.java
+++ bundles/treetank-core/src/main/java/com/treetank/package-info.java
@@ -16,7 +16,20 @@
  */
 
 /**
- * Root package for testcases containing Utility-classes.
+ * Root package for all packages. This includes:
+ * <ul>
+ * <li>access: Direct access layer for interaction with treetank</li>
+ * <li>api: Interfaces for offering abstract implementations</li>
+ * <li>axis: Different Axis for tree iterations</li>
+ * <li>cache: Caching and transaction log classes</li>
+ * <li>exception: Exception classes</li>
+ * <li>io: classes for writing data to the disk</li>
+ * <li>node: Nodelayer, all nodes are represented by this classes</li>
+ * <li>page: Pagelayer, all revisions which are storing nodes are represented by these classes</li>
+ * <li>service: XML + meta structures</li>
+ * <li>settings: constant variables are defined over here</li>
+ * <li>util: the rest, what else.</li>
+ * </ul>
  * 
  * @author Sebastian Graf, University of Konstanz
  */
