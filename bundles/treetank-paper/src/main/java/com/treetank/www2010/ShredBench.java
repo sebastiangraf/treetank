695d92eb9e44ed2d1f7b869c1709afe81c1910b4
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2010-10-22 13:51:19 +0000

    ASSIGNED - task 784: Rolling-Merkle hashed structure https://projects.uni-konstanz.de/issues/show/784
    adapted generic xmark benchmark
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5605 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-paper/src/main/java/com/treetank/www2010/ShredBench.java
+++ bundles/treetank-paper/src/main/java/com/treetank/www2010/ShredBench.java
@@ -14,6 +14,7 @@ import com.treetank.service.xml.shredder.XMLShredder;
 import org.perfidix.Benchmark;
 import org.perfidix.annotation.AfterEachRun;
 import org.perfidix.annotation.Bench;
+import org.perfidix.ouput.CSVOutput;
 import org.perfidix.ouput.TabularSummaryOutput;
 import org.perfidix.result.BenchmarkResult;
 
@@ -98,22 +99,25 @@ public class ShredBench {
     @AfterEachRun
     public void tearDown() {
         TestHelper.closeEverything();
-
-        System.out.println("Run finished!");
     }
 
     public static void main(final String[] args) {
 
-        if (args.length > 0) {
-            final File filetoshred = new File(args[0]);
-            ShredBench.XMLFile = filetoshred;
+        // Argument is a folder with only XML in there. For each XML one benchmark should be executed.
+        final File filetoshred = new File(args[0]);
+        final File[] files = filetoshred.listFiles();
+        final File filetoexport = new File(args[1]);
+        for (final File currentFile : files) {
+            final int index = currentFile.getName().lastIndexOf(".");
+            final File folder = new File(filetoexport, currentFile.getName().substring(0, index));
+            folder.mkdirs();
+            XMLFile = currentFile;
+            final Benchmark bench = new Benchmark();
+            bench.add(ShredBench.class);
+            final BenchmarkResult res = bench.run();
+            new CSVOutput(folder).visitBenchmark(res);
+            System.out.println("Finished benchmark for " + currentFile.getName());
         }
 
-        final Benchmark bench = new Benchmark();
-        bench.add(ShredBench.class);
-
-        final BenchmarkResult res = bench.run();
-        new TabularSummaryOutput().visitBenchmark(res);
     }
-
 }
