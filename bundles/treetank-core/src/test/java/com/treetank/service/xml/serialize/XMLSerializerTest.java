8b4f95bd867b39fecf4ed796e771b1ec1226a288
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2010-12-02 17:16:41 +0000

    fixed test
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5669 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/com/treetank/service/xml/serialize/XMLSerializerTest.java
+++ bundles/treetank-core/src/test/java/com/treetank/service/xml/serialize/XMLSerializerTest.java
@@ -23,6 +23,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
+import javax.management.modelmbean.XMLParseException;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.stream.XMLEventReader;
@@ -59,27 +60,6 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class XMLSerializerTest {
-
-    /**
-     * The resource name.
-     */
-    private static final transient String RESOURCENAME = "books";
-
-    /**
-     * The path where the databases will be stored.
-     */
-    private final static transient String STOREDBPATH =
-        File.separatorChar + "tmp" + File.separatorChar + "tt";
-
-    /**
-     * This field the begin result element of a XQuery or XPath expression.
-     */
-    private static transient String beginResult = "<jaxrx:result xmlns:jaxrx=\"http://jaxrx.org/\">";
-
-    /**
-     * This field the end result element of a XQuery or XPath expression.
-     */
-    private static transient String endResult = "</jaxrx:result>";
 
     @Before
     public void setUp() throws TreetankException {
@@ -185,172 +165,31 @@ public class XMLSerializerTest {
     @Test
     public void testKeyStart() throws Exception {
 
-        final File dir = new File(STOREDBPATH);
-        deleteDirectory(dir);
-        final InputStream xmlInput = XMLSerializerTest.class.getResourceAsStream("/books.xml");
-        shred(xmlInput, RESOURCENAME);
-        xmlInput.close();
-        String xPath = "//book";
-        OutputStream output = new ByteArrayOutputStream();
-        output.write(beginResult.getBytes());
+        final IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
+        final ISession session = database.getSession();
+        final IWriteTransaction wtx = session.beginWriteTransaction();
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
 
-        IDatabase database = null;
-        ISession session = null;
-        IReadTransaction rtx = null;
-        try {
-            database = Database.openDatabase(new File(STOREDBPATH + File.separatorChar + "books.tnk"));
-            // Creating a new session
-            session = database.getSession();
-            // Creating a transaction
-            rtx = session.beginReadTransaction();
+        // generate serialize all from this session
+        DocumentCreater.createVersioned(wtx);
+        wtx.commit();
+        wtx.close();
 
-            final IAxis axis = new XPathAxis(rtx, xPath);
-            for (final long key : axis) {
-                if (key >= 0) {
-                    final XMLSerializerProperties props = new XMLSerializerProperties();
-                    final XMLSerializerBuilder builder =
-                        new XMLSerializerBuilder(session, key, output, props);
+        XMLSerializer serializerall =
+            new XMLSerializerBuilder(session, 5l, out, new XMLSerializerProperties()).build();
+        serializerall.call();
+        final String result =
+            "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><b>\n  foo\n  <c/>\n</b>\n";
 
-                    builder.setREST(true);
-                    builder.setID(true);
-                    builder.setDeclaration(false);
-                    final XMLSerializer serializer = builder.build();
-                    serializer.call();
-                } else {
-                    output.write(rtx.getNode().getRawValue());
-                }
-            }
+        assertEquals(result, out.toString());
+        out.reset();
 
-        } catch (final TreetankException ttExcep) {
-            ttExcep.printStackTrace();
-        } catch (final IOException ioExcep) {
-            ioExcep.printStackTrace();
-        } catch (final Exception globExcep) {
-            globExcep.printStackTrace();
-        } finally {
-            rtx.moveToDocumentRoot();
-            synchronized (database) {
-                database.close();
-            }
-        }
+        serializerall = new XMLSerializerBuilder(session, out, 0, 1, 2).build();
+        serializerall.call();
+        assertEquals(DocumentCreater.VERSIONEDXML, out.toString());
+        session.close();
+        database.close();
 
-        output.write(endResult.getBytes());
-
-        InputStream xmlResult = new ByteArrayInputStream(((ByteArrayOutputStream)output).toByteArray());
-        Document resultDoc = xmlDocument(xmlResult);
-        final NodeList bNodes = resultDoc.getElementsByTagName("book");
-        assertEquals("test for items size of books is 6", 6, bNodes.getLength());
-
-    }
-
-    /**
-     * This method deletes a not empty directory.
-     * 
-     * @param path
-     *            The director that has to be deleted.
-     * @return <code>true</code> if the deletion process has been successful. <code>false</code> otherwise.
-     */
-    private static boolean deleteDirectory(final File path) {
-        if (path.exists()) {
-            final File[] files = path.listFiles();
-            for (int i = 0; i < files.length; i++) {
-                if (files[i].isDirectory()) {
-                    deleteDirectory(files[i]);
-                } else {
-                    files[i].delete();
-                }
-            }
-        }
-        return path.delete();
-    }
-
-    /**
-     * This method creates of an input stream an XML document.
-     * 
-     * @param input
-     *            The input stream.
-     * @return The packed XML document.
-     * @throws SAXException
-     *             Exception occurred.
-     * @throws IOException
-     *             Exception occurred.
-     * @throws ParserConfigurationException
-     *             Exception occurred.
-     */
-    private Document xmlDocument(final InputStream input) throws SAXException, IOException,
-        ParserConfigurationException {
-        return DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(input);
-    }
-
-    /**
-     * This method is responsible to save the XML file, which is in an {@link InputStream}, as a TreeTank
-     * object.
-     * 
-     * @param xmlInput
-     *            The XML file in an {@link InputStream}.
-     * @param resource
-     *            The name of the resource.
-     * @return <code>true</code> when the shredding process has been successful. <code>false</code> otherwise.
-     * @throws TreetankException
-     */
-    public final boolean shred(final InputStream xmlInput, final String resource) throws TreetankException {
-        boolean allOk = false;
-        IWriteTransaction wtx = null;
-        IDatabase database = null;
-        ISession session = null;
-        boolean abort = false;
-        try {
-            final StringBuilder tnkFileName = new StringBuilder(STOREDBPATH + File.separatorChar + resource);
-            tnkFileName.append(".tnk");
-            final File tnk = new File(tnkFileName.toString());
-
-            // Shredding the database to the file as XML
-            final Properties dbProps = new Properties();
-            dbProps.setProperty(EDatabaseSetting.REVISION_TO_RESTORE.name(), "1");
-            final DatabaseConfiguration conf = new DatabaseConfiguration(tnk, dbProps);
-
-            Database.createDatabase(conf);
-            database = Database.openDatabase(tnk);
-            session = database.getSession();
-            wtx = session.beginWriteTransaction();
-            wtx.moveToDocumentRoot();
-            final boolean exist = wtx.moveToFirstChild();
-            if (exist) {
-                wtx.remove();
-                wtx.commit();
-            }
-            final XMLShredder shredder = new XMLShredder(wtx, createReader(xmlInput), true);
-            shredder.call();
-            allOk = true;
-        } catch (final Exception exce) {
-            abort = true;
-        } finally {
-            synchronized (database) {
-                if (abort) {
-                    wtx.abort();
-                }
-                database.close();
-            }
-        }
-        return allOk;
-    }
-
-    /**
-     * This method creates an {@link XMLEventReader} out of an {@link InputStream}.
-     * 
-     * @param inputStream
-     *            The {@link InputStream} containing the XML file that has to be
-     *            stored.
-     * @return The {@link XMLStreamReader} object.
-     * @throws IOException
-     *             The exception occurred.
-     * @throws XMLStreamException
-     *             The exception occurred.
-     */
-    private XMLEventReader createReader(final InputStream inputStream) throws IOException, XMLStreamException {
-        final XMLInputFactory factory = XMLInputFactory.newInstance();
-        factory.setProperty(XMLInputFactory.SUPPORT_DTD, false);
-        return factory.createXMLEventReader(inputStream);
     }
 
 }
