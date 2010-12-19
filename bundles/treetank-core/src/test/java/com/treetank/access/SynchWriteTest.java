a6613723b1e7ebf1e994562e7e33d560b36d0f66
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2010-12-19 17:12:29 +0000

    refactoring testcase for concurrent
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5716 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/com/treetank/access/SynchWriteTest.java
+++ bundles/treetank-core/src/test/java/com/treetank/access/SynchWriteTest.java
@@ -1,8 +1,7 @@
-
 package com.treetank.access;
 
 import java.util.concurrent.Exchanger;
-
+import static org.junit.Assert.assertEquals;
 import javax.xml.namespace.QName;
 
 import junit.framework.TestCase;
@@ -22,17 +21,17 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-@Ignore public class SynchWriteTest extends TestCase {
-    Exchanger<Boolean> threadsFinished = new Exchanger();
-    Exchanger<Boolean> verify = new Exchanger();
+public class SynchWriteTest {
+    Exchanger<Boolean> threadsFinished = new Exchanger<Boolean>();
+    Exchanger<Boolean> verify = new Exchanger<Boolean>();
 
     @Before
     public void setUp() throws TreetankException {
         TestHelper.deleteEverything();
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
+        final IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
         final ISession session = database.getSession();
         final IWriteTransaction wtx = session.beginWriteTransaction();
-        DocumentCreater.createWithoutNamespace(wtx);
+        DocumentCreater.create(wtx);
         wtx.commit();
         wtx.close();
         session.close();
@@ -42,63 +41,11 @@ import org.junit.Test;
     public void tearDown() throws TreetankException {
         TestHelper.closeEverything();
     }
-    
-    @Test
-    public void testEmptyCommit() throws TreetankException{
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
-        final ISession session = database.getSession();
-        final IWriteTransaction wtx = session.beginWriteTransaction();
-        wtx.commit();
-        wtx.close();
-        session.close();
-        TestCase.assertTrue(true);
-    }
 
     @Test
-    public void testAbort() throws TreetankException{
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
-        final ISession session = database.getSession();
-        final IWriteTransaction wtx = session.beginWriteTransaction();
-        wtx.abort();
-        wtx.close();
-        session.close();
-        TestCase.assertTrue(true);
-    }
-
-    @Test
-    public void testAbortAfterCommit() throws TreetankException{
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
-        final ISession session = database.getSession();
-        final IWriteTransaction wtx = session.beginWriteTransaction();
-        wtx.commit();
-        wtx.abort();
-        wtx.close();
-        session.close();
-        TestCase.assertTrue(true);
-    }
-
-    @Test
-    public void testNewWtxAfterDirtyClose() throws TreetankException{
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
-        final ISession session = database.getSession();
-        final IWriteTransaction wtx = session.beginWriteTransaction();
-        wtx.insertElementAsFirstChild(new QName("test"));
-        wtx.close();
-        try {
-            IWriteTransaction wtx2 = session.beginWriteTransaction();
-            wtx2.insertElementAsFirstChild(new QName("should work"));
-            TestCase.assertTrue(true);
-        } catch (Exception e) {
-            TestCase.fail();
-        }
-
-        session.close();
-        TestCase.assertTrue(true);
-    }
-
-    @Test
-    public void testConcurrentWrite() throws TreetankException{
-        IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
+    @Ignore
+    public void testConcurrentWrite() throws TreetankException {
+        final IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
         final ISession session = database.getSession();
         final IWriteTransaction wtx = session.beginWriteTransaction();
         final IWriteTransaction wtx2 = session.beginWriteTransaction();
@@ -109,53 +56,48 @@ import org.junit.Test;
         t2.start();
 
         boolean ready = false;
-        while(!ready){
+        while (!ready) {
             try {
                 ready = verify.exchange(false);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
+            } catch (final InterruptedException e) {
+                throw new RuntimeException(e);
             }
         }
 
+        final IReadTransaction rtx = session.beginReadTransaction();
+        TestCase.assertTrue(rtx.moveToFirstChild());
+        TestCase.assertTrue(rtx.moveToFirstChild());
+        TestCase.assertTrue(rtx.moveToRightSibling());
+        TestCase.assertTrue(rtx.moveToFirstChild()); // should be at 6:foo
+        TestCase.assertEquals("foobar", rtx.getValueOfCurrentNode());
 
-        IReadTransaction rtx = session.beginReadTransaction();
-        try {
-            rtx = session.beginReadTransaction();
-            TestCase.assertTrue(rtx.moveToFirstChild());
-            TestCase.assertTrue(rtx.moveToFirstChild());
-            TestCase.assertTrue(rtx.moveToRightSibling());
-            TestCase.assertTrue(rtx.moveToFirstChild());        //should be at 6:foo
-            TestCase.assertEquals("foobar", rtx.getValueOfCurrentNode());
-
-            rtx.moveToDocumentRoot();
-            TestCase.assertTrue(rtx.moveToFirstChild());
-            TestCase.assertTrue(rtx.moveToFirstChild());
-            TestCase.assertTrue(rtx.moveToRightSibling());
-            TestCase.assertTrue(rtx.moveToRightSibling());
-            TestCase.assertTrue(rtx.moveToRightSibling());
-            TestCase.assertTrue(rtx.moveToFirstChild()); 
-            TestCase.assertTrue(rtx.moveToRightSibling()); //should be at 12:bar
-            TestCase.assertEquals("barfoo", rtx.getValueOfCurrentNode());
-        } catch (TreetankException e) {
-            TestCase.fail();
-        }
+        rtx.moveToDocumentRoot();
+        TestCase.assertTrue(rtx.moveToFirstChild());
+        TestCase.assertTrue(rtx.moveToFirstChild());
+        TestCase.assertTrue(rtx.moveToRightSibling());
+        TestCase.assertTrue(rtx.moveToRightSibling());
+        TestCase.assertTrue(rtx.moveToRightSibling());
+        TestCase.assertTrue(rtx.moveToFirstChild());
+        TestCase.assertTrue(rtx.moveToRightSibling()); // should be at 12:bar
+        TestCase.assertEquals("barfoo", rtx.getValueOfCurrentNode());
     }
 }
 
-class Wtx1 implements Runnable{
+class Wtx1 implements Runnable {
     IWriteTransaction wtx;
     Exchanger<Boolean> threadsFinished;
-    Wtx1(IWriteTransaction swtx, Exchanger<Boolean> threadsFinished){
+
+    Wtx1(IWriteTransaction swtx, Exchanger<Boolean> threadsFinished) {
         this.wtx = swtx;
         this.threadsFinished = threadsFinished;
     }
+
     @Override
-    public void run(){
+    public void run() {
         wtx.moveToFirstChild();
         wtx.moveToFirstChild();
         wtx.moveToRightSibling();
-        wtx.moveToFirstChild();        //should be at 6:foo
+        wtx.moveToFirstChild(); // should be at 6:foo
         try {
             wtx.setValue("foobar");
             wtx.commit();
@@ -175,26 +117,28 @@ class Wtx1 implements Runnable{
     }
 }
 
-class Wtx2 implements Runnable{
+class Wtx2 implements Runnable {
 
     IWriteTransaction wtx;
     boolean t1ready = false;
     Exchanger threadsFinished;
     Exchanger verify;
-    Wtx2(IWriteTransaction swtx, Exchanger<Boolean> threadsFinished, Exchanger<Boolean> verify){
+
+    Wtx2(IWriteTransaction swtx, Exchanger<Boolean> threadsFinished, Exchanger<Boolean> verify) {
         this.wtx = swtx;
         this.verify = verify;
         this.threadsFinished = threadsFinished;
     }
+
     @Override
-    public void run(){
+    public void run() {
         wtx.moveToFirstChild();
         wtx.moveToFirstChild();
         wtx.moveToRightSibling();
         wtx.moveToRightSibling();
         wtx.moveToRightSibling();
-        wtx.moveToFirstChild(); 
-        wtx.moveToRightSibling(); //should be at 12:bar
+        wtx.moveToFirstChild();
+        wtx.moveToRightSibling(); // should be at 12:bar
         try {
             wtx.setValue("barfoo");
             wtx.commit();
@@ -205,7 +149,7 @@ class Wtx2 implements Runnable{
             e.printStackTrace();
         }
 
-        while(!t1ready){        //waiting for t1 to finish
+        while (!t1ready) { // waiting for t1 to finish
             try {
                 t1ready = (Boolean)threadsFinished.exchange(false);
             } catch (InterruptedException e) {
@@ -214,15 +158,10 @@ class Wtx2 implements Runnable{
         }
 
         try {
-            verify.exchange(true);  //signaling that t2 has finished after t1 has finished
+            verify.exchange(true); // signaling that t2 has finished after t1 has finished
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
 }
-
-
-
-
-
