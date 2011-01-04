e78ed3e021216b6ab0409712365c2beb575cec81
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2011-01-04 16:47:26 +0000

    finished seriliazer
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5744 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-paper/src/main/java/com/treetank/xmlprague/SerializeBench.java
+++ bundles/treetank-paper/src/main/java/com/treetank/xmlprague/SerializeBench.java
@@ -4,17 +4,22 @@ import java.io.File;
 
 import com.treetank.TestHelper;
 import com.treetank.TestHelper.PATHS;
+import com.treetank.access.Database;
 import com.treetank.api.IDatabase;
 import com.treetank.api.IReadTransaction;
 import com.treetank.api.ISession;
-import com.treetank.api.IWriteTransaction;
 import com.treetank.exception.TTException;
 import com.treetank.service.xml.serialize.XMLSerializer;
-import com.treetank.service.xml.shredder.EShredderInsert;
-import com.treetank.service.xml.shredder.XMLShredder;
+import com.treetank.service.xml.serialize.XMLSerializer.XMLSerializerBuilder;
 
+import org.perfidix.AbstractConfig;
 import org.perfidix.Benchmark;
 import org.perfidix.annotation.Bench;
+import org.perfidix.element.KindOfArrangement;
+import org.perfidix.meter.AbstractMeter;
+import org.perfidix.meter.Time;
+import org.perfidix.meter.TimeMeter;
+import org.perfidix.ouput.AbstractOutput;
 import org.perfidix.ouput.CSVOutput;
 import org.perfidix.ouput.TabularSummaryOutput;
 import org.perfidix.result.BenchmarkResult;
@@ -22,34 +27,26 @@ import org.perfidix.result.BenchmarkResult;
 public class SerializeBench {
 
     private XMLSerializer serializer;
-    private static int RUNS;
+    public static File TNKFILE = new File("src" + File.separator + "main" + File.separator + "resources"
+        + File.separator + "small.xml");
 
     private long revisionKey = 0;
-
-    static {
-        try {
-            final IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
-            final ISession session = database.getSession();
-            final IReadTransaction rtx = session.beginReadTransaction();
-            RUNS = (int)rtx.getRevisionNumber();
-            database.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public void beforeSerialize() {
         try {
             final IDatabase database = TestHelper.getDatabase(PATHS.PATH1.getFile());
             final ISession session = database.getSession();
 
-//            serializer = new XMLSerializer(session, 0, revisionKey);
+            final XMLSerializerBuilder builder =
+                new XMLSerializerBuilder(session, new NullOutputStream(), revisionKey);
+
+            serializer = builder.build();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    @Bench(beforeEachRun = "beforeShred", beforeFirstRun = "beforeFirstShred", afterEachRun = "tearDown")
+    @Bench(beforeEachRun = "beforeSerialize", afterEachRun = "tearDown")
     public void benchInsert() {
         try {
             serializer.call();
@@ -60,35 +57,52 @@ public class SerializeBench {
 
     public void tearDown() {
         TestHelper.closeEverything();
+        revisionKey++;
     }
 
     public static void main(final String[] args) {
 
-//        if (args.length != 2) {
-//            System.out
-//                .println("Please use java -jar JAR \"folder with xmls to parse\" \"folder to write csv\"");
-//            System.exit(-1);
-//        }
-//
-//        // Argument is a folder with only XML in there. For each XML one benchmark should be executed.
-//        final File filetoshred = new File(args[0]);
-//        final File[] files = filetoshred.listFiles();
-//        final File filetoexport = new File(args[1]);
-//        for (final File currentFile : files) {
-//            XMLFile = currentFile;
-//            System.out.println("Starting benchmark for " + XMLFile.getName());
-//            final int index = currentFile.getName().lastIndexOf(".");
-//            final File folder = new File(filetoexport, currentFile.getName().substring(0, index));
-//            folder.mkdirs();
-//
-//            final Benchmark bench = new Benchmark();
-//            bench.add(ShredBench.class);
-//            final BenchmarkResult res = bench.run();
-//            new TabularSummaryOutput(System.out).visitBenchmark(res);
-//            new CSVOutput(folder).visitBenchmark(res);
-//            System.out.println("Finished benchmark for " + XMLFile.getName());
-//        }
+        if (args.length != 2) {
+            System.out
+                .println("Please use java -jar JAR \"folder with tnks to serialize\" \"folder to write csv\"");
+            System.exit(-1);
+        }
+
+        // Argument is a folder with only XML in there. For each XML one benchmark should be executed.
+        final File filetoserialize = new File(args[0]);
+        final File[] files = filetoserialize.listFiles();
+        final File filetoexport = new File(args[1]);
+        for (final File currentFile : files) {
+            TNKFILE = currentFile;
+            System.out.println("Starting benchmark for " + TNKFILE.getName());
+            final int index = currentFile.getName().lastIndexOf(".");
+            final File folder = new File(filetoexport, currentFile.getName().substring(0, index));
+            folder.mkdirs();
+            final int runs = (int)getRevisions(TNKFILE);
+
+            final Benchmark bench = new Benchmark(new AbstractConfig(runs, new AbstractMeter[] {
+                new TimeMeter(Time.MilliSeconds)
+            }, new AbstractOutput[0], KindOfArrangement.SequentialMethodArrangement, 1.0d) {
+            });
+            final BenchmarkResult res = bench.run();
+            new TabularSummaryOutput(System.out).visitBenchmark(res);
+            new CSVOutput(folder).visitBenchmark(res);
+            System.out.println("Finished benchmark for " + TNKFILE.getName());
+        }
 
     }
 
+    private static long getRevisions(final File tnks) {
+        try {
+            final IDatabase database = Database.openDatabase(tnks);
+            final ISession session = database.getSession();
+            final IReadTransaction rtx = session.beginReadTransaction();
+            final long returnVal = rtx.getRevisionNumber();
+            Database.forceCloseDatabase(tnks);
+            return returnVal;
+        } catch (TTException exc) {
+            exc.printStackTrace();
+            return 0;
+        }
+    }
 }
