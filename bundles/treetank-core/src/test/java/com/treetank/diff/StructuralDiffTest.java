3de316362386b5199761611ad2c149b8ab00cc7e
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2011-01-18 13:39:43 +0000

    ASSIGNED - task 1002: Diff package in Treetank https://projects.uni-konstanz.de/issues/show/1002
    New testcase added.
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5769 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/com/treetank/diff/StructuralDiffTest.java
+++ bundles/treetank-core/src/test/java/com/treetank/diff/StructuralDiffTest.java
@@ -18,12 +18,21 @@ package com.treetank.diff;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.xml.stream.XMLStreamException;
+
 import com.treetank.TestHelper;
 import com.treetank.api.IDatabase;
+import com.treetank.api.IWriteTransaction;
 import com.treetank.exception.TTException;
+import com.treetank.service.xml.shredder.EShredderCommit;
+import com.treetank.service.xml.shredder.EShredderInsert;
+import com.treetank.service.xml.shredder.XMLShredder;
+import com.treetank.service.xml.shredder.XMLUpdateShredder;
 import com.treetank.utils.DocumentCreater;
 
 import org.junit.After;
@@ -34,15 +43,17 @@ import org.junit.Test;
  * @author Johannes Lichtenberger, University of Konstanz
  * 
  */
-public class StructuralDiffTest implements IDiffObserver {
+public final class StructuralDiffTest implements IDiffObserver {
     private transient IDatabase mDatabase;
 
     private transient EDiff mDiff;
-    
+
     private transient int mCounter;
 
+    private static final String RESOURCES = "src" + File.separator + "test" + File.separator + "resources";
+
     @Before
-    public final void setUp() throws TTException {
+    public void setUp() throws TTException {
         TestHelper.deleteEverything();
         mDatabase = TestHelper.getDatabase(TestHelper.PATHS.PATH1.getFile());
         DocumentCreater.createVersioned(mDatabase.getSession().beginWriteTransaction());
@@ -50,16 +61,16 @@ public class StructuralDiffTest implements IDiffObserver {
     }
 
     @After
-    public final void tearDown() {
+    public void tearDown() {
         TestHelper.closeEverything();
     }
 
     @Test
-    public final void testStructuralDiff() {
+    public void testStructuralDiffFirst() {
         final Set<IDiffObserver> observer = new HashSet<IDiffObserver>();
         observer.add(this);
         DiffFactory.invokeStructuralDiff(mDatabase, 0, 1, 0, EDiffKind.NORMAL, observer);
-        
+
         while (mDiff != EDiff.DONE) {
             switch (mCounter) {
             case 1:
@@ -67,7 +78,48 @@ public class StructuralDiffTest implements IDiffObserver {
                 break;
             case 2:
                 assertEquals(mDiff, EDiff.INSERTED);
-                break;     
+                break;
+            default:
+                assertEquals(mDiff, EDiff.SAME);
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void testStructuralDiffSecond() throws TTException, IOException, XMLStreamException {
+        TestHelper.closeEverything();
+        TestHelper.deleteEverything();
+        final IDatabase database = TestHelper.getDatabase(TestHelper.PATHS.PATH1.getFile());
+        final IWriteTransaction wtx = database.getSession().beginWriteTransaction();
+        final XMLShredder init =
+            new XMLShredder(wtx, XMLShredder.createReader(new File(RESOURCES + File.separator + "revXMLsAll4"
+                + File.separator + "1.xml")), EShredderInsert.ADDASFIRSTCHILD);
+        init.call();
+        final File file = new File(RESOURCES + File.separator + "revXMLsAll4" + File.separator + "2.xml");
+        final XMLShredder shredder =
+            new XMLUpdateShredder(wtx, XMLShredder.createReader(file), EShredderInsert.ADDASFIRSTCHILD, file,
+                EShredderCommit.COMMIT);
+        shredder.call();
+        
+        final Set<IDiffObserver> observer = new HashSet<IDiffObserver>();
+        observer.add(this);
+        DiffFactory.invokeStructuralDiff(database, 0, 1, 0, EDiffKind.NORMAL, observer);
+
+        while (mDiff != EDiff.DONE) {
+            switch (mCounter) {
+            case 1:
+                assertEquals(mDiff, EDiff.SAME);
+                break;
+            case 2:
+                assertEquals(mDiff, EDiff.SAME);
+                break;
+            case 3:
+                assertEquals(mDiff, EDiff.INSERTED);
+                break;
+            case 4:
+                assertEquals(mDiff, EDiff.DELETED);
+                break;
             default:
                 assertEquals(mDiff, EDiff.SAME);
                 break;
@@ -76,7 +128,7 @@ public class StructuralDiffTest implements IDiffObserver {
     }
 
     @Override
-    public final void diffListener(final EDiff paramDiff) {
+    public void diffListener(final EDiff paramDiff) {
         mDiff = paramDiff;
         mCounter++;
     }
