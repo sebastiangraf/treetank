389ed1b078c24dc8e05040017bb06c84a5482950
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2011-02-08 13:25:02 +0000

    added content
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5793 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ bundles/treetank-cdl/src/main/java/com/treetank/cdl/ImportXML.java
@@ -0,0 +1,68 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.treetank.cdl;
+
+import java.io.File;
+
+import javax.xml.stream.XMLEventReader;
+
+import com.treetank.access.Database;
+import com.treetank.api.IDatabase;
+import com.treetank.api.ISession;
+import com.treetank.api.IWriteTransaction;
+import com.treetank.service.xml.shredder.EShredderInsert;
+import com.treetank.service.xml.shredder.XMLShredder;
+
+/**
+ * Die class acts as an import for XML into Treetank.
+ */
+public final class ImportXML {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.out.println("Usage: java -jar CDL \"XMLToImport.xml\" \"TTToStore.tnk\"");
+            System.exit(-1);
+        }
+
+        // File setup
+        final File importFile = new File(args[0]);
+        final File storeFile = new File(args[1]);
+
+        // Wtx setup
+        final IDatabase db = Database.openDatabase(storeFile);
+        final ISession session = db.getSession();
+        final IWriteTransaction wtx = session.beginWriteTransaction();
+
+        // Stax Setup
+        final XMLEventReader reader = XMLShredder.createReader(importFile);
+
+        final XMLShredder shredder = new XMLShredder(wtx, reader, EShredderInsert.ADDASFIRSTCHILD);
+        shredder.call();
+
+        wtx.commit();
+        wtx.close();
+        session.close();
+        db.close();
+    }
+
+}
