a4f8592ea2372cd538b7a8a843a28863fb4c4af4
Johannes Lichtenberger <johannes.lichtenberger@uni-konstanz.de>
2011-04-02 13:13:57 +0000

    ASSIGNED - task 428: Gui Skeleton https://projects.uni-konstanz.de/issues/show/428
    Fixed zooming/panning (now works without buffered image). After resetting a new buffered image is being created.
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5963 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ bundles/treetank-gui/src/main/java/org/treetank/gui/view/IVisualItem.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.treetank.gui.view;
+
+/**
+ * Interface to describe a visual item.
+ * 
+ * @author Johannes Lichtenberger, University of Konstanz
+ *
+ */
+public interface IVisualItem {
+    /** Update an item. */
+    void update();
+}
