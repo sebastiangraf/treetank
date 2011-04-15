def33bfa04900e48b5e44156ece8bfcf931013c1
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2011-04-15 13:44:31 +0000

    ASSIGNED - task 1448: Not working with Maven 3 https://projects.uni-konstanz.de/issues/show/1448
    testing completed
    
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5991 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/org/treetank/access/SessionTest.java
+++ bundles/treetank-core/src/test/java/org/treetank/access/SessionTest.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2011, University of Konstanz, Distributed Systems Group
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the University of Konstanz nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the University of Konstanz nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -38,7 +38,6 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
-
 
 import org.treetank.TestHelper;
 import org.treetank.TestHelper.PATHS;
@@ -93,38 +92,6 @@ public class SessionTest {
         database.close();
 
         assertEquals(null, database.getFile());
-    }
-
-    @Test
-    @Ignore
-    public void testNoWritesBeforeFirstCommit() throws AbsTTException {
-        // ISession session = Session
-        // .beginSession(ITestConstants.TEST_INSERT_CHILD_PATH);
-        // assertEquals(0L, new File(ITestConstants.TEST_INSERT_CHILD_PATH
-        // + File.separator + "tt.tnk").length());
-        // session.close();
-        // assertEquals(0L, new File(ITestConstants.TEST_INSERT_CHILD_PATH
-        // + File.separator + "tt.tnk").length());
-        //
-        // session =
-        // Session.beginSession(ITestConstants.TEST_INSERT_CHILD_PATH);
-        // assertEquals(0L, new File(ITestConstants.TEST_INSERT_CHILD_PATH
-        // + File.separator + "tt.tnk").length());
-        //
-        // final IWriteTransaction wtx = session.beginWriteTransaction();
-        // wtx.commit();
-        // wtx.close();
-        // session.close();
-        //
-        // session =
-        // Session.beginSession(ITestConstants.TEST_INSERT_CHILD_PATH);
-        // final IReadTransaction rtx = session.beginReadTransaction();
-        // rtx.close();
-        // session.close();
-        //
-        // TestCase.assertNotSame(0L, new File(
-        // ITestConstants.TEST_INSERT_CHILD_PATH + File.separator
-        // + "tt.tnk").length());
     }
 
     @Test
