d9267d93eb0a5a7f282b5dcbf2e79f311e05da56
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2011-04-18 11:53:04 +0000

    script for updating only
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@5996 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- scripts/update.sh
+++ scripts/update.sh
@@ -11,9 +11,3 @@ hg pull ssh://sebastiangraf@treetank.hg.sourceforge.net/hgroot/treetank/treetank
 hg update
 hg merge 2>/dev/null
 hg commit -m "merged disy and sf data"
-
-#building maven stuff
-mvn clean deploy -U
-
-# comitting changes
-hg push
