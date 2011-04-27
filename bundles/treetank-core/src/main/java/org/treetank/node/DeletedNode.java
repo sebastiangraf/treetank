9f46d652fc40add2ea8c0c30a4c817700d43595a
patrick.3.lang <patrick.3.lang@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2011-04-27 12:35:20 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6037 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ bundles/treetank-core/src/main/java/org/treetank/node/DeletedNode.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2011, University of Konstanz, Distributed Systems Group
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the University of Konstanz nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.treetank.node;
+
+import org.treetank.api.IItem;
+import org.treetank.api.IReadTransaction;
+import org.treetank.api.IVisitor;
+import org.treetank.io.ITTSink;
+
+/**
+ * If a node is deleted, it will be encapsulated over this class.
+ * 
+ * @author Sebastian Graf
+ * 
+ */
+public final class DeletedNode extends AbsNode {
+
+    DeletedNode(final long[] mLongBuilder, final int[] mIntBuilder) {
+        super(mLongBuilder, mIntBuilder);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void serialize(final ITTSink mNodeOut) {
+        super.serialize(mNodeOut);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ENodes getKind() {
+        return ENodes.DELETE_KIND;
+    }
+
+    @Override
+    public AbsNode clone() {
+        final AbsNode toClone = new DeletedNode(ENodes.cloneData(mLongData), ENodes.cloneData(mIntData));
+        return toClone;
+    }
+
+    public static AbsNode createData(final long mNodeKey, final long parentKey) {
+        final long[] longData = new long[ENodes.DELETE_KIND.getLongSize()];
+        final int[] intData = new int[ENodes.DELETE_KIND.getIntSize()];
+        longData[AbsNode.NODE_KEY] = mNodeKey;
+        longData[AbsNode.PARENT_KEY] = parentKey;
+        return new DeletedNode(longData, intData);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T extends IItem> T accept(final IReadTransaction paramTransaction) {
+        return (T)paramTransaction.getNode(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void acceptVisitor(final IVisitor paramVisitor) {
+        // Do nothing.
+    }
+}
---
+++ bundles/treetank-core/src/main/java/org/treetank/node/io/NodeInputSource.java
@@ -0,0 +1,125 @@
+/**
+ * Copyright (c) 2011, Distributed Systems Group, University of Konstanz
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * 
+ */
+package org.treetank.node.io;
+
+import java.io.ByteArrayInputStream;
+
+import org.treetank.io.ITTSource;
+
+/**
+ * {@link NodeSource} implementation for reading node input.
+ * 
+ * @author Patrick Lang, University of Konstanz
+ * 
+ */
+public class NodeInputSource implements ITTSource {
+
+    /**
+     * Input stream for node source.
+     */
+    private final ByteArrayInputStream mNodeInput;
+
+    /**
+     * Offset for byte operations.
+     */
+    private int mOffset;
+
+    /**
+     * Constructor.
+     */
+    public NodeInputSource(final byte[] mByteStream) {
+        this.mNodeInput = new ByteArrayInputStream(mByteStream);
+        mOffset = 0;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public long readLong() {
+        final byte[] mLongBytes = new byte[8];
+
+        final int mStatus = mNodeInput.read(mLongBytes, mOffset, 8);
+
+        if (mStatus < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return byteArrayToLong(mLongBytes);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public byte readByte() {
+
+        final int mNextByte = mNodeInput.read();
+
+        if (mNextByte < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return (byte)mNextByte;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public int readInt() {
+        final byte[] mIntBytes = new byte[4];
+
+        final int mStatus = mNodeInput.read(mIntBytes, mOffset, 4);
+
+        if (mStatus < 0) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return byteArrayToInt(mIntBytes);
+    }
+
+    /**
+     * Converting a byte array to integer.
+     * 
+     * @param mByteArray
+     *            Byte array to convert.
+     * @return converted integer value.
+     */
+    private int byteArrayToInt(final byte[] mByteArray) {
+        final int mConvInt =
+            ((mByteArray[0] & 0xff) << 24) | ((mByteArray[1] & 0xff) << 16) | ((mByteArray[2] & 0xff) << 8)
+                | (mByteArray[3] & 0xff);
+
+        return mConvInt;
+    }
+
+    /**
+     * Converting a byte array to long.
+     * 
+     * @param mByteArray
+     *            Byte array to convert.
+     * @return converted long value.
+     */
+    private long byteArrayToLong(final byte[] mByteArray) {
+        final long mConvLong =
+            ((long)(mByteArray[0] & 0xff) << 56) | ((long)(mByteArray[1] & 0xff) << 48)
+                | ((long)(mByteArray[2] & 0xff) << 40) | ((long)(mByteArray[3] & 0xff) << 32)
+                | ((long)(mByteArray[4] & 0xff) << 24) | ((long)(mByteArray[5] & 0xff) << 16)
+                | ((long)(mByteArray[6] & 0xff) << 8) | ((long)(mByteArray[7] & 0xff));
+
+        return mConvLong;
+    }
+
+}
