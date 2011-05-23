c63871623d2543407e2cf14ce1d343ce80504a3c
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2011-05-23 12:07:03 +0000

    adding branched-adaption to enable diff-testcases
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6124 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/org/treetank/diff/FullDiffTest.java
+++ bundles/treetank-core/src/test/java/org/treetank/diff/FullDiffTest.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2011, University of Konstanz, Distributed Systems Group
  * All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *     * Redistributions of source code must retain the above copyright
- *       notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright
- *       notice, this list of conditions and the following disclaimer in the
- *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the University of Konstanz nor the
- *       names of its contributors may be used to endorse or promote products
- *       derived from this software without specific prior written permission.
- *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the University of Konstanz nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -76,19 +76,17 @@ public class FullDiffTest {
 
     private static final long TIMEOUT_S = 200000;
 
-    @Ignore
     @Before
     public final void setUp() throws AbsTTException {
         mStart = new CountDownLatch(1);
         TestHelper.deleteEverything();
     }
-    
-    @Ignore
+
     @After
     public final void tearDown() {
         TestHelper.closeEverything();
     }
-    @Ignore
+
     @Test
     public void testFullDiffFirst() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -127,7 +125,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffOptimizedFirst() throws InterruptedException, AbsTTException {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -152,12 +150,13 @@ public class FullDiffTest {
 
         final Set<IDiffObserver> observer = new HashSet<IDiffObserver>();
         observer.add(listener);
-        DiffFactory.invokeFullDiff(new DiffFactory.Builder(database, 0, 1, 0, EDiffOptimized.HASHED, observer));
+        DiffFactory
+            .invokeFullDiff(new DiffFactory.Builder(database, 0, 1, 0, EDiffOptimized.HASHED, observer));
 
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffSecond() throws AbsTTException, IOException, XMLStreamException,
         InterruptedException {
@@ -199,9 +198,10 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
-    public void testFullDiffThird() throws AbsTTException, IOException, XMLStreamException, InterruptedException {
+    public void testFullDiffThird() throws AbsTTException, IOException, XMLStreamException,
+        InterruptedException {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
         listener.diffListener(eq(EDiff.SAME), isA(IItem.class), isA(IItem.class), isA(DiffDepth.class));
         listener.diffListener(eq(EDiff.DELETED), isA(IItem.class), isA(IItem.class), isA(DiffDepth.class));
@@ -242,7 +242,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffFourth() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -282,7 +282,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffFifth() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -318,7 +318,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffSixth() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -357,7 +357,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffSeventh() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -402,7 +402,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public void testFullDiffEighth() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
@@ -447,7 +447,7 @@ public class FullDiffTest {
         mStart.await(TIMEOUT_S, TimeUnit.SECONDS);
         verify(listener);
     }
-    @Ignore
+
     @Test
     public final void testFullDiffTenth() throws Exception {
         final IDiffObserver listener = createStrictMock(IDiffObserver.class);
