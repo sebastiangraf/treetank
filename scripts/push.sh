9e39b4f1b934fbab0ed6de2f8dcab36eb560acfd
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2011-08-29 14:34:48 +0000

    push script adapted
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6291 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 #setting error to false, not beautiful but efficient because of updating/merging
 set +e
 #submitting to sourceforge
-hg push
+hg push ssh://sebastiangraf@treetank.hg.sourceforge.net/hgroot/treetank/treetank
 exit 0
