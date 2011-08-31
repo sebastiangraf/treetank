341e86f00816fe050e9d1f3d9c1ad80ce0432e8b
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2011-08-31 09:19:25 +0000

    adapted script with usage of hg fetch extension and removal if fetching the sf-data
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6297 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -5,11 +5,6 @@
 set +e
 #getting disy data
 hg pull
-hg update
-hg merge 2>/dev/null
-#getting sf data
-hg pull ssh://sebastiangraf@treetank.hg.sourceforge.net/hgroot/treetank/treetank
-hg update
-hg merge 2>/dev/null
-hg commit -m "merged disy and sf data"
+#getting sf data at the moment is to complicated, only push to sf supported
+#hg pull ssh://sebastiangraf@treetank.hg.sourceforge.net/hgroot/treetank/treetank
 exit 0
