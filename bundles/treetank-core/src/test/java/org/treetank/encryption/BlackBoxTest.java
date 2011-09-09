e21d5ee692b6b68d280485d271050384b006065c
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2011-09-09 08:14:42 +0000

    excluding non-compiling test
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6317 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-core/src/test/java/org/treetank/encryption/BlackBoxTest.java
+++ bundles/treetank-core/src/test/java/org/treetank/encryption/BlackBoxTest.java
@@ -19,7 +19,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class BlackBoxTest {
-
+/*
     private static EncryptionHelper enHelper;
 
     private static Holder holder;
@@ -92,6 +92,6 @@ public class BlackBoxTest {
             "disy"
         });
 
-    }
+    }*/
 
 }
