d4f369efe7174feb593a182e5ce65e15a0dc9db5
Patrick Lang <patrick.3.lang@uni-konstanz.de>
2011-10-18 20:04:01 +0000

    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6357 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- bundles/treetank-encryption/src/test/java/org/treetank/encryption/benchmarking/old/BenchPerformanceUser.java
+++ bundles/treetank-encryption/src/test/java/org/treetank/encryption/benchmarking/old/BenchPerformanceUser.java
@@ -1,4 +1,29 @@
-package org.treetank.encryption.benchmarking;
+/**
+ * Copyright (c) 2011, University of Konstanz, Distributed Systems Group
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the University of Konstanz nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */package org.treetank.encryption.benchmarking.old;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,20 +35,25 @@ import java.util.LinkedList;
 
 import org.treetank.encryption.EncryptionController;
 import org.treetank.encryption.EncryptionOperator;
+import org.treetank.encryption.utils.BenchUtils;
 import org.treetank.exception.TTEncryptionException;
 
-public class BenchPerformanceDataset {
+/**
+ * Class to test performance for an number of users.
+ * 
+ * @author Patrick Lang, University of Konstanz
+ * 
+ */
+public class BenchPerformanceUser {
 
     private static final String DATAFILE = "src" + File.separator + "test" + File.separator + "resources"
-        + File.separator + "testdata.txt";
+        + File.separator + "testdataUser.txt";
 
     private static int joinOps = 0;
     private static int leaveOps = 0;
-    private static int dataCounter = 0;
+    private static int counter = 0;
 
-    private static int dataRuns = 10000;
-
-    private static long time = 0;
+    private final static int userRuns = 50;
 
     public static void main(String[] args) {
         try {
@@ -31,11 +61,11 @@ public class BenchPerformanceDataset {
             new EncryptionController().setEncryptionOption(true);
             new EncryptionController().init();
 
-            time = System.currentTimeMillis();
+            long time = System.currentTimeMillis();
             System.out.println("Started...");
             init();
             System.out.println("Ended...");
-            System.out.println("Parsed lines: " + dataCounter);
+            System.out.println("Parsed lines: " + counter);
             System.out.println("Total time needed: " + (System.currentTimeMillis() - time) + "ms");
             System.out.println("Total joins: " + joinOps);
             System.out.println("Total leaves: " + leaveOps);
@@ -61,16 +91,11 @@ public class BenchPerformanceDataset {
 
             EncryptionOperator op;
 
-            while (((line = in.readLine()) != null && dataCounter < dataRuns)) {
-                op = new EncryptionOperator();
-                dataCounter++;
+            int userCounter = 0;
 
-                if (dataCounter % 1000 == 0) {
-                    System.out.println(dataCounter);
-                    System.out.println("Time needed: " + (System.currentTimeMillis() - time) + "ms");
-                    System.out.println("Joins: " + joinOps + " Leaves: " + leaveOps);
-                    System.out.println("");
-                }
+            while (((line = in.readLine()) != null && userCounter < userRuns)) {
+                op = new EncryptionOperator();
+                counter++;
 
                 final String[] dataArray = new BenchUtils().parseData(line);
 
@@ -86,60 +111,16 @@ public class BenchPerformanceDataset {
 
                     String user = newGroups[newGroups.length - 1];
                     String group = newGroups[newGroups.length - 2];
-                    if (op.nodeExists(user)) {
 
-                        if (op.nodeExists(group)) {
-                            // if user and group exist, and user is not member of group, add user to group
-                            if (!op.checkMembership(user, group)) {
-                                op.join(group, new String[] {
-                                    user
-                                });
-                                joinOps++;
-                            }
-
-                        } else {
-                            // if user exist but not group, check which groups of hierarchy already exist,
-                            // create it
-                            // and add user.
-                            LinkedList<String> groupList = new LinkedList<String>();
-                            String parent = "ROOT";
-                            for (int i = 0; i < newGroups.length - 1; i++) {
-                                if (!op.nodeExists(newGroups[i])) {
-                                    groupList.add(newGroups[i]);
-                                } else {
-                                    parent = newGroups[i];
-                                }
-                                // groupList.add(newGroups[newGroups.length-1]);
-                            }
-
-                            if (groupList.size() == newGroups.length - 1) {
-                                op.join("ROOT", newGroups);
-                                joinOps++;
-                                op.join(newGroups[newGroups.length - 1], new String[] {
-                                    user
-                                });
-                                joinOps++;
-                            } else {
-                                if (groupList.size() > 1) {
-                                    op.join(parent, groupList.toArray(new String[0]));
-                                    joinOps++;
-                                    op.join(newGroups[newGroups.length - 1], new String[] {
-                                        user
-                                    });
-                                    joinOps++;
-                                }
-                            }
-                        }
-
-                    } else {
-                        // if user not exists, but group exists, add user to group
+                    //if (!op.userExists(user)) { //check not further necessary, after building a unique user data set
+                        userCounter++;
                         if (op.nodeExists(group)) {
                             op.join(group, new String[] {
                                 user
                             });
                             joinOps++;
                         } else { // if user and group does not exist, check which groups of hierarchy exists,
-                            // create it and add user.
+                                 // create it and add user.
                             LinkedList<String> groupList = new LinkedList<String>();
                             String parent = "ROOT";
                             for (int i = 0; i < newGroups.length - 1; i++) {
@@ -160,11 +141,11 @@ public class BenchPerformanceDataset {
                                     joinOps++;
                                 }
                             }
-                        }
+                        //}
 
                     }
-
                 }
+
             }
 
         } catch (final FileNotFoundException e) {
@@ -174,5 +155,4 @@ public class BenchPerformanceDataset {
         }
 
     }
-
 }
--- bundles/treetank-encryption/src/test/java/org/treetank/encryption/benchmarking/old/BenchUpdateOnDAG.java
+++ bundles/treetank-encryption/src/test/java/org/treetank/encryption/benchmarking/old/BenchUpdateOnDAG.java
@@ -1,4 +1,29 @@
-package org.treetank.encryption.benchmarking;
+/**
+ * Copyright (c) 2011, University of Konstanz, Distributed Systems Group
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * * Neither the name of the University of Konstanz nor the
+ * names of its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */package org.treetank.encryption.benchmarking.old;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -19,6 +44,8 @@ import org.perfidix.ouput.TabularSummaryOutput;
 import org.perfidix.result.BenchmarkResult;
 import org.treetank.encryption.EncryptionController;
 import org.treetank.encryption.EncryptionOperator;
+import org.treetank.encryption.benchmarking.KtsMeter;
+import org.treetank.encryption.utils.BenchUtils;
 import org.treetank.exception.TTEncryptionException;
 
 /**
@@ -132,7 +159,6 @@ public class BenchUpdateOnDAG {
                     System.out.println("User: " + userCounter);
                     System.out.println("Time needed Leave: " + timeNeeded + " ms");
                     System.out.println("Key Trails: " + KtsMeter.getInstance().getValue() + " kts");
-                    System.out.println("Nodes affected: " + op.getAffectedNodes().size());
                     System.out.println("DAG Size: " + EncryptionController.getInstance().getDAGDb().count());
                     System.out
                         .println("DAG Revisions Size: " + new EncryptionController().getSelDb().count());
