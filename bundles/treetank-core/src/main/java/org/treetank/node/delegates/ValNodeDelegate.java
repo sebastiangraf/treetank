397f0a98ad5527a8083a2074e23cf236e06d3fc0
Sebastian Graf <sebastian.graf@uni-konstanz.de>
2011-11-10 18:47:10 +0000

    ASSIGNED - task 1406: Node + Nodepage refactoring https://projects.uni-konstanz.de/issues/show/1406
    ValNodeDel and NameNode Interface, only adding, not using
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6407 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ bundles/treetank-core/src/main/java/org/treetank/node/delegates/ValNodeDelegate.java
@@ -0,0 +1,12 @@
+package org.treetank.node.delegates;
+
+import org.treetank.api.IVisitor;
+import org.treetank.io.ITTSink;
+import org.treetank.node.ENodes;
+import org.treetank.node.interfaces.IValNode;
+
+public class ValNodeDelegate/* implements IValNode */{
+
+    private NodeDelegate mDelegate;
+
+}
