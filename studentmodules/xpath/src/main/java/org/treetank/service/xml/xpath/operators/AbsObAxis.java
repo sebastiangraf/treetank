38ddab01f014af2ee9615b294988ad41ce2291e2
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2012-03-21 08:13:50 +0000

    ASSIGNED - task 1406: Separation of node layer, page layer and storage layer https://projects.uni-konstanz.de/issues/show/1406
    [RM] multiple functions in the Functions.class which are all static but never needed any more (Functions were never supported)
    
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6664 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- studentmodules/xpath/src/main/java/org/treetank/service/xml/xpath/operators/AbsObAxis.java
+++ studentmodules/xpath/src/main/java/org/treetank/service/xml/xpath/operators/AbsObAxis.java
@@ -47,181 +47,167 @@ import org.treetank.service.xml.xpath.types.Type;
  */
 public abstract class AbsObAxis extends AbsAxis {
 
-	/** First arithmetic operand. */
-	private final AbsAxis mOperand1;
+    /** First arithmetic operand. */
+    private final AbsAxis mOperand1;
 
-	/** Second arithmetic operand. */
-	private final AbsAxis mOperand2;
+    /** Second arithmetic operand. */
+    private final AbsAxis mOperand2;
 
-	/** True, if axis has not been evaluated yet. */
-	private boolean mIsFirst;
+    /** True, if axis has not been evaluated yet. */
+    private boolean mIsFirst;
 
-	private List<AtomicValue> mToStore;
+    private List<AtomicValue> mToStore;
 
-	/**
-	 * Constructor. Initializes the internal state.
-	 * 
-	 * @param rtx
-	 *            Exclusive (immutable) trx to iterate with.
-	 * @param mOp1
-	 *            First value of the operation
-	 * @param mOp2
-	 *            Second value of the operation
-	 */
-	public AbsObAxis(final INodeReadTransaction rtx, final AbsAxis mOp1,
-			final AbsAxis mOp2, final List<AtomicValue> pToStore) {
+    /**
+     * Constructor. Initializes the internal state.
+     * 
+     * @param rtx
+     *            Exclusive (immutable) trx to iterate with.
+     * @param mOp1
+     *            First value of the operation
+     * @param mOp2
+     *            Second value of the operation
+     */
+    public AbsObAxis(final INodeReadTransaction rtx, final AbsAxis mOp1, final AbsAxis mOp2,
+        final List<AtomicValue> pToStore) {
 
-		super(rtx);
-		mOperand1 = mOp1;
-		mOperand2 = mOp2;
-		mIsFirst = true;
-		mToStore = pToStore;
+        super(rtx);
+        mOperand1 = mOp1;
+        mOperand2 = mOp2;
+        mIsFirst = true;
+        mToStore = pToStore;
 
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public final void reset(final long mNodeKey) {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public final void reset(final long mNodeKey) {
 
-		super.reset(mNodeKey);
-		mIsFirst = true;
-		if (mOperand1 != null) {
-			mOperand1.reset(mNodeKey);
-		}
+        super.reset(mNodeKey);
+        mIsFirst = true;
+        if (mOperand1 != null) {
+            mOperand1.reset(mNodeKey);
+        }
 
-		if (mOperand2 != null) {
-			mOperand2.reset(mNodeKey);
-		}
-	}
+        if (mOperand2 != null) {
+            mOperand2.reset(mNodeKey);
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean hasNext() {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasNext() {
 
-		resetToLastKey();
+        resetToLastKey();
 
-		if (mIsFirst) {
-			mIsFirst = false;
+        if (mIsFirst) {
+            mIsFirst = false;
 
-			if (mOperand1.hasNext()) {
-				// atomize operand
-				final AtomicValue mItem1 = atomize(mOperand1);
+            if (mOperand1.hasNext()) {
+                // atomize operand
+                final AtomicValue mItem1 = atomize(mOperand1);
 
-				if (mOperand2.hasNext()) {
-					// atomize operand
-					final AtomicValue mItem2 = atomize(mOperand2);
-					try {
-						final AtomicValue result = operate(mItem1, mItem2);
-						// add retrieved AtomicValue to item list
-						final int itemKey = getTransaction().getItemList()
-								.addItem(result);
-						mToStore.add(result);
-						getTransaction().moveTo(itemKey);
+                if (mOperand2.hasNext()) {
+                    // atomize operand
+                    final AtomicValue mItem2 = atomize(mOperand2);
+                    try {
+                        final AtomicValue result = operate(mItem1, mItem2);
+                        // add retrieved AtomicValue to item list
+                        mToStore.add(result);
+                        final int itemKey = getTransaction().getItemList().addItem(result);
+                        getTransaction().moveTo(itemKey);
 
-						return true;
-					} catch (TTXPathException e) {
-						throw new RuntimeException(e);
-					}
-				}
-			}
+                        return true;
+                    } catch (TTXPathException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
 
-			if (XPATH_10_COMP) { // and empty sequence, return NaN
-				final AtomicValue result = new AtomicValue(Double.NaN,
-						Type.DOUBLE);
-				mToStore.add(result);
-				final int itemKey = getTransaction().getItemList().addItem(
-						result);
-				getTransaction().moveTo(itemKey);
-				return true;
-			}
+            if (XPATH_10_COMP) { // and empty sequence, return NaN
+                final AtomicValue result = new AtomicValue(Double.NaN, Type.DOUBLE);
+                mToStore.add(result);
+                final int itemKey = getTransaction().getItemList().addItem(result);
+                getTransaction().moveTo(itemKey);
+                return true;
+            }
 
-		}
-		// either not the first call, or empty sequence
-		resetToStartKey();
-		return false;
+        }
+        // either not the first call, or empty sequence
+        resetToStartKey();
+        return false;
 
-	}
+    }
 
-	/**
-	 * Atomizes an operand according to the rules specified in the XPath
-	 * specification.
-	 * 
-	 * @param mOperand
-	 *            the operand to atomize
-	 * @return the atomized operand. (always an atomic value)
-	 */
-	private AtomicValue atomize(final AbsAxis mOperand) {
+    /**
+     * Atomizes an operand according to the rules specified in the XPath
+     * specification.
+     * 
+     * @param mOperand
+     *            the operand to atomize
+     * @return the atomized operand. (always an atomic value)
+     */
+    private AtomicValue atomize(final AbsAxis mOperand) {
 
-		final INodeReadTransaction rtx = getTransaction();
-		int type = rtx.getNode().getTypeKey();
-		AtomicValue atom;
+        final INodeReadTransaction rtx = getTransaction();
+        int type = rtx.getNode().getTypeKey();
+        AtomicValue atom;
 
-		if (XPATH_10_COMP) {
-			if (type == rtx.keyForName("xs:double")
-					|| type == rtx.keyForName("xs:untypedAtomic")
-					|| type == rtx.keyForName("xs:boolean")
-					|| type == rtx.keyForName("xs:string")
-					|| type == rtx.keyForName("xs:integer")
-					|| type == rtx.keyForName("xs:float")
-					|| type == rtx.keyForName("xs:decimal")) {
-				Function.fnnumber(mOperand.getTransaction(), mToStore);
-			}
+        if (XPATH_10_COMP) {
 
-			atom = new AtomicValue(rtx.getValueOfCurrentNode().getBytes(), rtx
-					.getNode().getTypeKey());
-		} else {
-			// unatomicType is cast to double
-			if (type == rtx.keyForName("xs:untypedAtomic")) {
-				type = rtx.keyForName("xs:double");
-				// TODO: throw error, of cast fails
-			}
+            atom = new AtomicValue(rtx.getValueOfCurrentNode().getBytes(), rtx.getNode().getTypeKey());
+        } else {
+            // unatomicType is cast to double
+            if (type == rtx.keyForName("xs:untypedAtomic")) {
+                type = rtx.keyForName("xs:double");
+                // TODO: throw error, of cast fails
+            }
 
-			atom = new AtomicValue(rtx.getValueOfCurrentNode().getBytes(), type);
-		}
+            atom = new AtomicValue(rtx.getValueOfCurrentNode().getBytes(), type);
+        }
 
-		// if (!XPATH_10_COMP && operand.hasNext()) {
-		// throw new XPathError(ErrorType.XPTY0004);
-		// }
+        // if (!XPATH_10_COMP && operand.hasNext()) {
+        // throw new XPathError(ErrorType.XPTY0004);
+        // }
 
-		return atom;
-	}
+        return atom;
+    }
 
-	/**
-	 * Performs the operation on the two input operands. First checks if the
-	 * types of the operands are a valid combination for the operation and if so
-	 * computed the result. Otherwise an XPathError is thrown.
-	 * 
-	 * @param mOperand1
-	 *            first input operand
-	 * @param mOperand2
-	 *            second input operand
-	 * @return result of the operation
-	 * @throws TTXPathException
-	 *             if the operations fails
-	 */
-	protected abstract AtomicValue operate(final AtomicValue mOperand1,
-			final AtomicValue mOperand2) throws TTXPathException;
+    /**
+     * Performs the operation on the two input operands. First checks if the
+     * types of the operands are a valid combination for the operation and if so
+     * computed the result. Otherwise an XPathError is thrown.
+     * 
+     * @param mOperand1
+     *            first input operand
+     * @param mOperand2
+     *            second input operand
+     * @return result of the operation
+     * @throws TTXPathException
+     *             if the operations fails
+     */
+    protected abstract AtomicValue operate(final AtomicValue mOperand1, final AtomicValue mOperand2)
+        throws TTXPathException;
 
-	/**
-	 * Checks if the types of the operands are a valid combination for the
-	 * operation and if so returns the corresponding result type. Otherwise an
-	 * XPathError is thrown. This typed check is done according to the <a
-	 * href="http://www.w3.org/TR/xpath20/#mapping">Operator Mapping</a>.
-	 * 
-	 * @param mOp1
-	 *            first operand's type key
-	 * @param mOp2
-	 *            second operand's type key
-	 * @return return type of the arithmetic function according to the operand
-	 *         type combination.
-	 * @throws TTXPathException
-	 *             if type is not specified
-	 */
-	protected abstract Type getReturnType(final int mOp1, final int mOp2)
-			throws TTXPathException;
+    /**
+     * Checks if the types of the operands are a valid combination for the
+     * operation and if so returns the corresponding result type. Otherwise an
+     * XPathError is thrown. This typed check is done according to the <a
+     * href="http://www.w3.org/TR/xpath20/#mapping">Operator Mapping</a>.
+     * 
+     * @param mOp1
+     *            first operand's type key
+     * @param mOp2
+     *            second operand's type key
+     * @return return type of the arithmetic function according to the operand
+     *         type combination.
+     * @throws TTXPathException
+     *             if type is not specified
+     */
+    protected abstract Type getReturnType(final int mOp1, final int mOp2) throws TTXPathException;
 
 }
