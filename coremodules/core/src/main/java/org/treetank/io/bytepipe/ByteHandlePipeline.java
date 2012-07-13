33dd91b3b508e60289d798b858fe37087f10f7de
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2012-07-13 14:03:32 +0000

    [ADD] Pipeline for handling the bytes
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6895 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ coremodules/core/src/main/java/org/treetank/io/bytepipe/ByteHandlePipeline.java
@@ -0,0 +1,61 @@
+/**
+ * 
+ */
+package org.treetank.io.bytepipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.treetank.exception.TTByteHandleException;
+
+/**
+ * 
+ * Pipeline to handle Bytes before stored in the backends.
+ * 
+ * @author Sebastian Graf, University of Konstanz
+ * 
+ */
+public final class ByteHandlePipeline implements IByteHandler {
+
+    /** Pipeline hold over here. */
+    private final List<IByteHandler> mParts;
+
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param pParts
+     *            to be stored, Order is important!
+     */
+    public ByteHandlePipeline(final IByteHandler... pParts) {
+        mParts = new ArrayList<IByteHandler>();
+        for (IByteHandler part : pParts) {
+            mParts.add(part);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public byte[] serialize(final byte[] pToSerialize) throws TTByteHandleException {
+        byte[] pipeData = pToSerialize;
+        for (IByteHandler part : mParts) {
+            pipeData = part.serialize(pipeData);
+        }
+        return pipeData;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public byte[] deserialize(final byte[] pToDeserialize) throws TTByteHandleException {
+        byte[] pipeData = pToDeserialize;
+        for (int i = mParts.size()-1; i >= 0; i--) {
+            pipeData = mParts.get(i).deserialize(pipeData);
+        }
+        return pipeData;
+    }
+
+}
