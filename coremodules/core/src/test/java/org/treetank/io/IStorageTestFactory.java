a84c33ceb49306876ac9c4018a49dc356279acd4
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2012-08-04 09:02:35 +0000

    [ADD] Factory-test for IStorage
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@6925 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ coremodules/core/src/test/java/org/treetank/io/IStorageTestFactory.java
@@ -0,0 +1,76 @@
+package org.treetank.io;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Guice;
+import org.testng.annotations.Test;
+import org.treetank.TestHelper;
+import org.treetank.exception.TTException;
+import org.treetank.page.PageReference;
+import org.treetank.page.UberPage;
+
+import com.google.inject.Inject;
+
+@Guice(moduleFactory = ModuleFactory.class)
+public class IStorageTestFactory {
+
+    @Inject
+    private IStorageFactory mFac;
+
+    @BeforeMethod
+    public void setUp() throws TTException {
+        TestHelper.closeEverything();
+        TestHelper.deleteEverything();
+        TestHelper.PATHS.PATH1.getFile().mkdirs();
+    }
+
+    @AfterMethod
+    public void tearDown() throws TTException {
+        TestHelper.closeEverything();
+        TestHelper.deleteEverything();
+    }
+
+    /**
+     * Test method for {@link org.treetank.io.bytepipe.IByteHandler#deserialize(byte[])} and for
+     * {@link org.treetank.io.bytepipe.IByteHandler#serialize(byte[])}.
+     * 
+     * @throws TTException
+     */
+    @Test
+    public void testFirstRef() throws TTException {
+
+        IStorage handler = mFac.create(TestHelper.PATHS.PATH1.getFile());
+
+        final PageReference pageRef1 = new PageReference();
+        final UberPage page1 = new UberPage();
+        pageRef1.setPage(page1);
+
+        // same instance check
+        final IWriter writer = handler.getWriter();
+        writer.writeFirstReference(pageRef1);
+        final PageReference pageRef2 = writer.readFirstReference();
+        assertEquals(
+            new StringBuilder("Check for ").append(handler.getClass()).append(" failed.").toString(),
+            pageRef1.getNodePageKey(), pageRef2.getNodePageKey());
+        assertEquals(
+            new StringBuilder("Check for ").append(handler.getClass()).append(" failed.").toString(),
+            ((UberPage)pageRef1.getPage()).getRevisionCount(), ((UberPage)pageRef2.getPage())
+                .getRevisionCount());
+        writer.close();
+
+        // new instance check
+        final IReader reader = handler.getReader();
+        final PageReference pageRef3 = reader.readFirstReference();
+        assertEquals(
+            new StringBuilder("Check for ").append(handler.getClass()).append(" failed.").toString(),
+            pageRef1.getNodePageKey(), pageRef3.getNodePageKey());
+        assertEquals(
+            new StringBuilder("Check for ").append(handler.getClass()).append(" failed.").toString(),
+            ((UberPage)pageRef1.getPage()).getRevisionCount(), ((UberPage)pageRef3.getPage())
+                .getRevisionCount());
+        reader.close();
+        handler.close();
+    }
+}
---
+++ coremodules/core/src/test/java/org/treetank/io/ModuleFactory.java
@@ -0,0 +1,39 @@
+package org.treetank.io;
+
+import org.testng.IModuleFactory;
+import org.testng.ITestContext;
+import org.treetank.api.INodeFactory;
+import org.treetank.io.bytepipe.IByteHandler;
+import org.treetank.io.bytepipe.Zipper;
+import org.treetank.io.file.FileStorage;
+import org.treetank.page.DumbNodeFactory;
+import org.treetank.revisioning.FullDump;
+import org.treetank.revisioning.IRevisioning;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Module;
+import com.google.inject.assistedinject.FactoryModuleBuilder;
+
+public class ModuleFactory implements IModuleFactory {
+
+    @Override
+    public Module createModule(ITestContext context, Class<?> testClass) {
+
+        return new AbstractModule() {
+
+            @Override
+            protected void configure() {
+                bind(IRevisioning.class).to(FullDump.class);
+
+                bind(INodeFactory.class).to(DumbNodeFactory.class);
+                bind(IByteHandler.class).to(Zipper.class);
+
+                install(new FactoryModuleBuilder().implement(IStorage.class, FileStorage.class).build(
+                    IStorageFactory.class));
+
+            }
+        };
+
+    }
+
+}
