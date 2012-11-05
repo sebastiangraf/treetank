ef0b994e427c47a3e618e8c87df212e087ab2446
andreas.rain <andreas.rain@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2012-11-05 19:44:03 +0000

    [MOD] Implemented the INode interface using the class ByteNode and the INodeFactory using the class ByteNodeFactory.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@7021 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ interfacemodules/iscsi/src/main/java/org/treetank/iscsi/node/ByteNodeFactory.java
@@ -0,0 +1,14 @@
+package org.treetank.iscsi.node;
+
+import org.treetank.api.INode;
+import org.treetank.api.INodeFactory;
+
+public class ByteNodeFactory implements INodeFactory {
+
+	@Override
+	public INode deserializeNode(byte[] pData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
