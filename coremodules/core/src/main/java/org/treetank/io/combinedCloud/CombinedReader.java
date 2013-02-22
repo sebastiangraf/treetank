191fbb0156d5fcac6321a49d5620ae682d7d2fc7
sebastian.graf <sebastian.graf@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2013-02-22 15:22:11 +0000

    [ADD] Combined Backend conisting of two layers of backends, bdb and jclouds, neither tested nor functional at the moment
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@7268 e3ddb328-5bfe-0310-b762-aafcbcbd2528

---
+++ coremodules/core/src/main/java/org/treetank/io/combinedCloud/CombinedReader.java
@@ -0,0 +1,69 @@
+package org.treetank.io.combinedCloud;
+
+import org.treetank.exception.TTIOException;
+import org.treetank.io.IBackendReader;
+import org.treetank.page.UberPage;
+import org.treetank.page.interfaces.IPage;
+
+/**
+ * 
+ * Reader for a combined storage.
+ * 
+ * @author Sebastian Graf, University of Konstanz
+ * 
+ */
+public class CombinedReader implements IBackendReader {
+
+    /** First reader. */
+    private final IBackendReader mFirstReader;
+
+    /** Second reader. */
+    private final IBackendReader mSecondReader;
+
+    /**
+     * Constructor.
+     * 
+     * @param pFirstReader
+     *            First reader to read the data from
+     * @param pSecondReader
+     *            Second reader to read the data from, fallback from the first reader
+     */
+    public CombinedReader(final IBackendReader pFirstReader, final IBackendReader pSecondReader) {
+        this.mFirstReader = pFirstReader;
+        this.mSecondReader = pSecondReader;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public IPage read(long pKey) throws TTIOException {
+        IPage returnVal = mFirstReader.read(pKey);
+        if (returnVal == null) {
+            returnVal = mSecondReader.read(pKey);
+        }
+        return returnVal;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public UberPage readUber() throws TTIOException {
+        UberPage returnVal = mFirstReader.readUber();
+        if (returnVal == null) {
+            returnVal = mSecondReader.readUber();
+        }
+        return returnVal;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void close() throws TTIOException {
+        mFirstReader.close();
+        mSecondReader.close();
+    }
+
+}
