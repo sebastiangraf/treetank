3f5de526190d8c1b55771b76be5ab28e378bbcf5
andreas.rain <andreas.rain@e3ddb328-5bfe-0310-b762-aafcbcbd2528>
2013-08-25 12:11:38 +0000

    [MOD] JClouds seems to be too slow in combination with treetank. Trying a new FileStorage class and its performance.
    git-svn-id: https://svn.uni-konstanz.de/disy/Treetank/trunk/treetank@7538 e3ddb328-5bfe-0310-b762-aafcbcbd2528

--- interfacemodules/iscsi/src/main/java/org/treetank/jscsi/TreetankStorageModule.java
+++ interfacemodules/iscsi/src/main/java/org/treetank/jscsi/TreetankStorageModule.java
@@ -26,30 +26,18 @@ package org.treetank.jscsi;
 
 import static com.google.common.base.Preconditions.checkState;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.file.FileSystems;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
-import org.jclouds.ContextBuilder;
-import org.jclouds.blobstore.BlobStore;
-import org.jclouds.blobstore.BlobStoreContext;
-import org.jclouds.blobstore.domain.Blob;
 import org.jclouds.filesystem.reference.FilesystemConstants;
 import org.jscsi.target.storage.IStorageModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.treetank.access.IscsiWriteTrx;
-import org.treetank.access.conf.ConstructorProps;
 import org.treetank.api.IData;
 import org.treetank.api.IIscsiWriteTrx;
 import org.treetank.api.ISession;
@@ -67,59 +55,55 @@ import com.google.common.io.Files;
  */
 public class TreetankStorageModule implements IStorageModule {
 
-	/**
-	 * Number of Blocks in one Cluster. 8 equals 4KB nodes 16 equals 8KB nodes
-	 * ...
-	 * 
-	 * When using berkeley db as a backend consider that if you increase the
-	 * size of the blocks in node up to 12800 nodes can be held in ram. If you
-	 * have a 1gb storage maximally 1gb heap is needed. However if the storage
-	 * is bigger and the blocks per node is considerably high (e.g. 256kb per
-	 * node) you might have to increase the ram for the jvm.
-	 */
-	public static final int BLOCKS_IN_NODE = 16;
+    /**
+     * Number of Blocks in one Cluster. 8 equals 4KB nodes 16 equals 8KB nodes
+     * ...
+     * 
+     * When using berkeley db as a backend consider that if you increase the
+     * size of the blocks in node up to 12800 nodes can be held in ram. If you
+     * have a 1gb storage maximally 1gb heap is needed. However if the storage
+     * is bigger and the blocks per node is considerably high (e.g. 256kb per
+     * node) you might have to increase the ram for the jvm.
+     */
+    public static final int BLOCKS_IN_NODE = 16;
 
-	/** Threshold when commit should occur in number of bytes. */
-	private static final int COMMIT_THRESHOLD = 268435456;
+    /** Threshold when commit should occur in number of bytes. */
+    private static final int COMMIT_THRESHOLD = 268435456;
 
-	/** Number of Bytes in Bucket. */
-	public final static int BYTES_IN_NODE = BLOCKS_IN_NODE * VIRTUAL_BLOCK_SIZE;
+    /** Number of Bytes in Bucket. */
+    public final static int BYTES_IN_NODE = BLOCKS_IN_NODE * VIRTUAL_BLOCK_SIZE;
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(TreetankStorageModule.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TreetankStorageModule.class);
 
-	/**
-	 * The number of nodes in the storage resulting in mNodeNumbers *
-	 * BLOCKS_IN_NODE * VIRTUAL_BLOCK_SIZE bytes
-	 * 
-	 * @see #VIRTUAL_BLOCK_SIZE
-	 */
-	private final long mNodeNumbers;
+    /**
+     * The number of nodes in the storage resulting in mNodeNumbers *
+     * BLOCKS_IN_NODE * VIRTUAL_BLOCK_SIZE bytes
+     * 
+     * @see #VIRTUAL_BLOCK_SIZE
+     */
+    private final long mNodeNumbers;
 
-	/**
-	 * The mSession this storage module uses to access the storage device.
-	 */
-	private final ISession mSession;
+    /**
+     * The mSession this storage module uses to access the storage device.
+     */
+    private final ISession mSession;
 
-	/**
-	 * {@link IIscsiWriteTrx} that is used to write/read from treetank.
-	 */
-	private final IIscsiWriteTrx mRtx;
+    /**
+     * {@link IIscsiWriteTrx} that is used to write/read from treetank.
+     */
+    private final IIscsiWriteTrx mRtx;
 
-	/**
-	 * Bytewriter counter - If a certain amount of bytes have been written, a
-	 * commit is made to treetank.
-	 */
-	private volatile int mByteCounter;
+    /**
+     * Bytewriter counter - If a certain amount of bytes have been written, a
+     * commit is made to treetank.
+     */
+    private volatile int mByteCounter;
 
-    /** JClouds mirror for faster response times (filesystem backend) */
-    private BlobStore mMirrorStorage;
-    private byte[] mCurrentBlobBytes;
-    private long mCurrentBlobPos = -1;
-    private Map<Integer, byte[]> mBlobCache;
+    /** FileStorage mirror for faster response times (filesystem backend) */
+    private FileStorage mFileStorage;
 
-	/** ExecutorService to perform WriteTasks */
-	private ExecutorService mWriteTaskExecutor;
+    /** ExecutorService to perform WriteTasks */
+    private ExecutorService mWriteTaskExecutor;
 
     /**
      * Creates a storage module that is used by the target to handle I/O.
@@ -133,145 +117,101 @@ public class TreetankStorageModule implements IStorageModule {
      */
     public TreetankStorageModule(final long pNodeNumber, final ISession pSession) throws TTException {
 
-		mNodeNumbers = pNodeNumber;
+        mNodeNumbers = pNodeNumber;
 
-		LOGGER.debug("Initializing storagemodule with: number of nodes="
-				+ mNodeNumbers + ", blockSize="
-				+ IStorageModule.VIRTUAL_BLOCK_SIZE);
+        LOGGER.debug("Initializing storagemodule with: number of nodes=" + mNodeNumbers + ", blockSize="
+            + IStorageModule.VIRTUAL_BLOCK_SIZE);
 
-		mSession = pSession;
-		mRtx = new IscsiWriteTrx(mSession.beginBucketWtx(), mSession);
+        mSession = pSession;
+        mRtx = new IscsiWriteTrx(mSession.beginBucketWtx(), mSession);
 
-		mWriteTaskExecutor = Executors.newSingleThreadExecutor(new WriteTaskThreadFactory());
+        mWriteTaskExecutor = Executors.newSingleThreadExecutor(new WriteTaskThreadFactory());
 
-		createStorage();
-		System.out.println("Device ready");
-	}
+        createStorage();
+        System.out.println("Device ready");
+    }
 
-	/**
-	 * Bootstrap a new device as a treetank storage using nodes to abstract the
-	 * device.
-	 * 
-	 * @throws IOException
-	 *             is thrown if a node couldn't be created due to errors in the
-	 *             backend.
-	 */
-	private void createStorage() throws TTException {
+    /**
+     * Bootstrap a new device as a treetank storage using nodes to abstract the
+     * device.
+     * 
+     * @throws IOException
+     *             is thrown if a node couldn't be created due to errors in the
+     *             backend.
+     */
+    private void createStorage() throws TTException {
 
-		LOGGER.debug("Creating storage with " + mNodeNumbers
-				+ " nodes containing " + BLOCKS_IN_NODE + " blocks with "
-				+ IStorageModule.VIRTUAL_BLOCK_SIZE + " bytes each.");
+        LOGGER.debug("Creating storage with " + mNodeNumbers + " nodes containing " + BLOCKS_IN_NODE
+            + " blocks with " + IStorageModule.VIRTUAL_BLOCK_SIZE + " bytes each.");
 
         // Creating random access file as mirror
         Properties properties = new Properties();
         properties.setProperty(FilesystemConstants.PROPERTY_BASEDIR, Files.createTempDir().getAbsolutePath());
 
-        mMirrorStorage =
-            ContextBuilder.newBuilder("filesystem").credentials("iscsi", "iscsi").overrides(properties)
-                .buildView(BlobStoreContext.class).getBlobStore();
-        mMirrorStorage.createContainerInLocation(null, "mirror");
-        mBlobCache = new HashMap<Integer, byte[]>();
+        try {
+            mFileStorage = new FileStorage(Files.createTempDir().getAbsolutePath() , BLOCKS_IN_NODE * VIRTUAL_BLOCK_SIZE);
+        } catch (IOException e) {
+            throw new TTException(e.getMessage()) {
+                private static final long serialVersionUID = 6736130138810572965L;
+            };
+        }
 
         IData data = this.mRtx.getCurrentNode();
 
-		if (data != null) {
-			return;
-		}
-		boolean hasNextNode = true;
+        if (data != null) {
+            return;
+        }
+        boolean hasNextNode = true;
 
-		for (int i = 0; i < mNodeNumbers; i++) {
-			if (i == mNodeNumbers - 1) {
-				hasNextNode = false;
-			}
+        for (int i = 0; i < mNodeNumbers; i++) {
+            if (i == mNodeNumbers - 1) {
+                hasNextNode = false;
+            }
 
-			// Bootstrapping nodes containing clusterSize -many blocks/sectors.
-			LOGGER.debug("Bootstraping node " + i + "\tof "
-					+ (mNodeNumbers - 1));
-			this.mRtx.bootstrap(new byte[TreetankStorageModule.BYTES_IN_NODE],
-					hasNextNode);
-		}
+            // Bootstrapping nodes containing clusterSize -many blocks/sectors.
+            LOGGER.debug("Bootstraping node " + i + "\tof " + (mNodeNumbers - 1));
+            this.mRtx.bootstrap(new byte[TreetankStorageModule.BYTES_IN_NODE], hasNextNode);
+        }
 
-		this.mRtx.commit();
+        this.mRtx.commit();
 
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public int checkBounds(long logicalBlockAddress, int transferLengthInBlocks) {
-		// Checking if the logical block address is out of bounds
-		if (logicalBlockAddress < 0 || logicalBlockAddress >= getSizeInBlocks()) {
-			return 1;
-		} else
-		// if the logical block address is in bounds but the transferlength
-		// either exceeds
-		// the device size or is faulty return 2
-		if (transferLengthInBlocks < 0
-				|| logicalBlockAddress + transferLengthInBlocks > getSizeInBlocks()) {
-			return 2;
-		} else {
-			return 0;
-		}
-	}
+    /**
+     * {@inheritDoc}
+     */
+    public int checkBounds(long logicalBlockAddress, int transferLengthInBlocks) {
+        // Checking if the logical block address is out of bounds
+        if (logicalBlockAddress < 0 || logicalBlockAddress >= getSizeInBlocks()) {
+            return 1;
+        } else
+        // if the logical block address is in bounds but the transferlength
+        // either exceeds
+        // the device size or is faulty return 2
+        if (transferLengthInBlocks < 0 || logicalBlockAddress + transferLengthInBlocks > getSizeInBlocks()) {
+            return 2;
+        } else {
+            return 0;
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public long getSizeInBlocks() {
-		return mNodeNumbers * BLOCKS_IN_NODE;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public long getSizeInBlocks() {
+        return mNodeNumbers * BLOCKS_IN_NODE;
+    }
 
     /**
      * {@inheritDoc}
      */
     public void read(byte[] bytes, long storageIndex) throws IOException {
-    	
+
         LOGGER.debug("Starting to read with param: " + "\nstorageIndex = " + storageIndex
             + "\nbytes.length = " + bytes.length);
 
-        int size = 65536;
-        long blobPos = storageIndex / size;
-        int storageOffset = (int)(storageIndex % size);
-
-        Blob blob = mMirrorStorage.getBlob("mirror", String.valueOf(blobPos));
-        
-        byte[] blobBytes = new byte[size];
-
-        if (mCurrentBlobPos != blobPos) {
-            if (mBlobCache.containsKey(blobPos)) {
-                mCurrentBlobPos = blobPos;
-                mCurrentBlobBytes = mBlobCache.get(blobPos);
-            } else {
-
-                if (mBlobCache.entrySet().size() > 1024) {
-                    mBlobCache.remove(mCurrentBlobPos);
-                }
-                
-                if(blob != null){
-                    blob.getPayload().getInput().read(blobBytes);
-                }
-                mCurrentBlobBytes = blobBytes;
-                mCurrentBlobPos = blobPos;
-
-                mBlobCache.put((int)mCurrentBlobPos, mCurrentBlobBytes);
-            }
-        }
-        
-        if((storageOffset + bytes.length) > size){
-            System.arraycopy(mCurrentBlobBytes, storageOffset, bytes, 0, size - storageOffset);
-            byte[] nextStep = new byte[bytes.length - (size - storageOffset)];
-            read(nextStep, storageIndex + (size - storageOffset));
-            System.arraycopy(nextStep, 0, bytes, size - storageOffset, nextStep.length);
-        }
-        else{
-            System.arraycopy(mCurrentBlobBytes, storageOffset, bytes, 0, bytes.length);
-        }
-
-        if(blob != null){
-            blob.getPayload().release();
-            blob.getPayload().close();
-        }
+        mFileStorage.read(bytes, storageIndex);
 
         // long startIndex = storageIndex / BYTES_IN_NODE;
         // int startIndexOffset = (int)(storageIndex % BYTES_IN_NODE);
@@ -317,62 +257,14 @@ public class TreetankStorageModule implements IStorageModule {
      * {@inheritDoc}
      */
     public void write(byte[] bytes, long storageIndex) throws IOException {
-    	
+
         LOGGER.debug("Starting to write with param: " + "\nstorageIndex = " + storageIndex
             + "\nbytes.length = " + bytes.length);
 
-        int size = 65536;
-        long blobPos = storageIndex / size;
-        int storageOffset = (int)(storageIndex % size);
-
-        Blob blob = mMirrorStorage.getBlob("mirror", String.valueOf(blobPos));
-        byte[] blobBytes = new byte[size];
-
-        if (blob != null) {
-            if (mCurrentBlobPos == blobPos && mCurrentBlobBytes != null) {
-                blobBytes = mCurrentBlobBytes;
-            } else {
-                if (mBlobCache.containsKey(blobPos)) {
-                    mCurrentBlobBytes = mBlobCache.get(blobPos);
-                    mCurrentBlobPos = blobPos;
-                } else {
-
-                    if (mBlobCache.entrySet().size() > 1024) {
-                        mBlobCache.remove(mCurrentBlobPos);
-                    }
-
-                    blob.getPayload().getInput().read(blobBytes);
-                    mCurrentBlobBytes = blobBytes;
-                    mCurrentBlobPos = blobPos;
-
-                    mBlobCache.put((int)mCurrentBlobPos, mCurrentBlobBytes);
-                }
-                mCurrentBlobPos = blobPos;
-            }
-        }
-        else{
-            mCurrentBlobBytes = blobBytes;
-            mCurrentBlobPos = blobPos;
-        }
-        
-        if((storageOffset + bytes.length) > size){
-            System.arraycopy(bytes, 0, mCurrentBlobBytes, storageOffset, size - storageOffset);
-            blob = mMirrorStorage.blobBuilder(String.valueOf(blobPos)).payload(mCurrentBlobBytes).build();
-            byte[] nextStep = new byte[bytes.length - (size - storageOffset)];
-            System.arraycopy(bytes, (size - storageOffset), nextStep, 0, bytes.length - (size - storageOffset));
-            write(nextStep, storageIndex + (size - storageOffset));
-        }
-        else{
-            System.arraycopy(bytes, 0, mCurrentBlobBytes, storageOffset, bytes.length);
-            blob = mMirrorStorage.blobBuilder(String.valueOf(blobPos)).payload(mCurrentBlobBytes).build();
-        }
-
-        mMirrorStorage.putBlob("mirror", blob);
-        blob.getPayload().release();
-        blob.getPayload().close();
+        mFileStorage.write(bytes, storageIndex);
 
         // Submitting into treetank
-//         mWriteTaskExecutor.submit(new WriteTask(bytes, storageIndex));
+         mWriteTaskExecutor.submit(new WriteTask(bytes, storageIndex));
     }
 
     /**
@@ -448,27 +340,26 @@ public class TreetankStorageModule implements IStorageModule {
         }
 
     }
-    
+
     private class WriteTaskThreadFactory implements ThreadFactory {
-    	   public Thread newThread(Runnable r) {
-    		 Thread t = new Thread(r);
-    		 t.setPriority(Thread.MIN_PRIORITY);
-    	     return t;
-    	   }
-    	 }
+        public Thread newThread(Runnable r) {
+            Thread t = new Thread(r);
+            t.setPriority(Thread.MIN_PRIORITY);
+            return t;
+        }
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void close() throws IOException {
+    /**
+     * {@inheritDoc}
+     */
+    public void close() throws IOException {
 
-		try {
-            mMirrorStorage.getContext().close();
-			mRtx.close();
+        try {
+            mRtx.close();
 
-		} catch (TTException exc) {
-			throw new IOException(exc);
-		}
-	}
+        } catch (TTException exc) {
+            throw new IOException(exc);
+        }
+    }
 
 }
